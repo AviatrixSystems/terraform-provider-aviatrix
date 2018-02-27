de64c6a71e26762401b2400fd5056b2ac515e203
Rakesh Ranjan <rakeshranjan.568@gmail.com>
2018-02-27 13:44:12 +0530

    Adding AWSPeer resource. Still has some issues due to mantis #5500. This resource will work properly only after the mantis gets fixed.

---
+++ aviatrix/resource_aws_peer.go
@@ -0,0 +1,125 @@
+package aviatrix
+
+import (
+	"fmt"
+	"github.com/AviatrixSystems/go-aviatrix/goaviatrix"
+	"github.com/hashicorp/terraform/helper/schema"
+	"log"
+	"strings"
+)
+
+func resourceAWSPeer() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAWSPeerCreate,
+		Read:   resourceAWSPeerRead,
+		Update: resourceAWSPeerUpdate,
+		Delete: resourceAWSPeerDelete,
+
+		Schema: map[string]*schema.Schema{
+			"account_name1": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"account_name2": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"vpc_id1": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"vpc_id2": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"vpc_reg1": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"vpc_reg2": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"rtb_list1": &schema.Schema{
+				Type:     schema.TypeList,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Optional: true,
+			},
+			"rtb_list2": &schema.Schema{
+				Type:     schema.TypeList,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Optional: true,
+			},
+		},
+	}
+}
+
+func resourceAWSPeerCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*goaviatrix.Client)
+	aws_peer := &goaviatrix.AWSPeer{
+		AccountName1: d.Get("account_name1").(string),
+		AccountName2: d.Get("account_name2").(string),
+		VpcID1:       d.Get("vpc_id1").(string),
+		VpcID2:       d.Get("vpc_id2").(string),
+		Region1:      d.Get("vpc_reg1").(string),
+		Region2:      d.Get("vpc_reg2").(string),
+	}
+
+	if _, ok := d.GetOk("rtb_list1"); ok {
+		aws_peer.RtbList1 = strings.Join(goaviatrix.ExpandStringList(d.Get("rtb_list1").([]interface{})), ",")
+	}
+
+	if _, ok := d.GetOk("rtb_list2"); ok {
+		aws_peer.RtbList2 = strings.Join(goaviatrix.ExpandStringList(d.Get("rtb_list2").([]interface{})), ",")
+	}
+	log.Printf("[INFO] Creating Aviatrix aws_peer: %#v", aws_peer)
+	id, err := client.CreateAWSPeer(aws_peer)
+	if err != nil {
+		return fmt.Errorf("Failed to create Aviatrix AWSPeer: %s", err)
+	}
+	d.SetId(id)
+	return nil
+	//return resourceAWSPeerRead(d, meta)
+}
+
+func resourceAWSPeerRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*goaviatrix.Client)
+	aws_peer := &goaviatrix.AWSPeer{
+		VpcID1: d.Get("vpc_id1").(string),
+		VpcID2: d.Get("vpc_id2").(string),
+	}
+	ap, err := client.GetAWSPeer(aws_peer)
+	if err != nil {
+		if err == goaviatrix.ErrNotFound {
+			d.SetId("")
+			return nil
+		}
+		return fmt.Errorf("Couldn't find Aviatrix AWSPeer: %s", err)
+	}
+	log.Printf("[TRACE] Reading aws_peer: %#v", ap)
+	if ap != nil {
+		d.Set("vpc_id1", ap.VpcID1)
+		d.Set("vpc_id2", ap.VpcID2)
+	}
+	return nil
+}
+
+func resourceAWSPeerUpdate(d *schema.ResourceData, meta interface{}) error {
+	return fmt.Errorf("AWSPeer cannot be updated. Delete and create new AWS peering.", err)
+}
+
+func resourceAWSPeerDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*goaviatrix.Client)
+	aws_peer := &goaviatrix.AWSPeer{
+		VpcID1: d.Get("vpc_id1").(string),
+		VpcID2: d.Get("vpc_id2").(string),
+	}
+
+	log.Printf("[INFO] Deleting Aviatrix aws_peer: %#v", aws_peer)
+
+	err := client.DeleteAWSPeer(aws_peer)
+	if err != nil {
+		return fmt.Errorf("Failed to delete Aviatrix AWSPeer: %s", err)
+	}
+	return nil
+}
---
+++ vendor/github.com/AviatrixSystems/go-aviatrix/goaviatrix/aws_peering.go
@@ -0,0 +1,111 @@
+package goaviatrix
+
+import (
+	"fmt"
+	"encoding/json"
+	"errors"
+	"log"
+	"regexp"
+)
+
+// AWSPeer simple struct to hold aws_peer details
+type AWSPeer struct {
+	Action                  string `form:"action,omitempty"`
+	CID                     string `form:"CID,omitempty"`
+	AccountName1            string `form:"peer1_account_name,omitempty"`
+	AccountName2            string `form:"peer2_account_name,omitempty"`
+	VpcID1                  string `form:"peer1_vpc_id,omitempty"`
+	VpcID2                  string `form:"peer2_vpc_id,omitempty"`
+	Region1                 string `form:"peer1_region,omitempty"`
+	Region2                 string `form:"peer2_region,omitempty"`
+	RtbList1                string `form:"peer1_rtb_id,omitempty"`
+	RtbList2                string `form:"peer2_rtb_id,omitempty"`
+}
+
+type AwsPeerAPIResp struct {
+	Return  bool   `json:"return"`
+	Reason  string `json:"reason"`
+	Results  map[string]string `json:"results"`
+}
+
+func (c *Client) CreateAWSPeer(aws_peer *AWSPeer) (string, error) {
+	aws_peer.CID=c.CID
+	aws_peer.Action="create_aws_peering"
+	resp,err := c.Post(c.baseURL, aws_peer)
+	if err != nil {
+		return "", err
+	}
+	var data AwsPeerAPIResp
+	if err = json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return "", err
+	}
+	if(!data.Return){
+		return "", errors.New(data.Reason)
+	}
+	r, _ := regexp.Compile(`pcx-\w+`)
+	id := r.FindString(data.Results["text"])
+	return id, nil
+}
+
+func (c *Client) GetAWSPeer(aws_peer *AWSPeer) (*AWSPeer, error) {
+	aws_peer.CID=c.CID
+	aws_peer.Action="list_aws_peerings"
+	path := c.baseURL + fmt.Sprintf("?CID=%s&action=%s", aws_peer.CID, aws_peer.Action)
+	resp,err := c.Get(path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	//Output result for this query cannot be unmarshalled
+	//easily into our defined struct AWSPeer.
+	//So using a map of string->interface{}
+	var data map[string]interface{}
+	if err = json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return nil, err
+	}
+	if _, ok := data["reason"]; ok {
+		log.Printf("[INFO] Couldn't find AWS peering between VPCs %s and %s: %s", aws_peer.VpcID1, aws_peer.VpcID2, data["reason"])
+		return nil, ErrNotFound
+	}
+	if val, ok := data["results"]; ok {
+		if pair_list, ok1 := val.(map[string]interface{})["pair_list"].([]interface{}); ok1 {
+			for i := range pair_list {
+				if pair_list[i].(map[string]interface{})["requester"].(map[string]interface{})["vpc_id"].(string) == aws_peer.VpcID1 && pair_list[i].(map[string]interface{})["accepter"].(map[string]interface{})["vpc_id"].(string) == aws_peer.VpcID2 {
+					aws_peer := &AWSPeer{
+						VpcID1: pair_list[i].(map[string]interface{})["requester"].(map[string]interface{})["vpc_id"].(string),
+						VpcID2: pair_list[i].(map[string]interface{})["accepter"].(map[string]interface{})["vpc_id"].(string),
+						AccountName1: pair_list[i].(map[string]interface{})["requester"].(map[string]interface{})["account_name"].(string),
+						AccountName2: pair_list[i].(map[string]interface{})["accepter"].(map[string]interface{})["account_name"].(string),
+						Region1: pair_list[i].(map[string]interface{})["requester"].(map[string]interface{})["region"].(string),
+						Region2: pair_list[i].(map[string]interface{})["accepter"].(map[string]interface{})["region"].(string),
+					}
+					return aws_peer, nil
+				}
+			}
+		}
+	}
+	log.Printf("[INFO] No AWS peering between VPC %s and %s is present.", aws_peer.VpcID1, aws_peer.VpcID2)
+	return nil, ErrNotFound
+}
+
+func (c *Client) UpdateAWSPeer(aws_peer *AWSPeer) (error) {
+	return nil
+}
+
+func (c *Client) DeleteAWSPeer(aws_peer *AWSPeer) (error) {
+	aws_peer.CID=c.CID
+	aws_peer.Action="delete_aws_peering"
+	resp,err := c.Post(c.baseURL, aws_peer)
+	if err != nil {
+		return err
+	}
+
+	var data APIResp
+	if err = json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return err
+	}
+	if(!data.Return){
+		return errors.New(data.Reason)
+	}
+	return nil
+}
