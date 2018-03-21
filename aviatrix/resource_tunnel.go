e398dd162d44ad3a91d76fdabad33530c86c0e84
edumanig <edumanig@gmail.com>
2018-03-21 10:10:43 -0700

    fix mantix #5738 (#9)

--- aviatrix/resource_tunnel.go
+++ aviatrix/resource_tunnel.go
@@ -128,10 +128,22 @@ func resourceTunnelDelete(d *schema.ResourceData, meta interface{}) error {
 	tunnel := &goaviatrix.Tunnel{
 		VpcName1: d.Get("vpc_name1").(string),
 		VpcName2: d.Get("vpc_name2").(string),
+                EnableHA: d.Get("enable_ha").(string), 
 	}
 
 	log.Printf("[INFO] Deleting Aviatrix tunnel: %#v", tunnel)
+        if enable_ha := d.Get("enable_ha").(string); enable_ha == "yes" {
+		// parse the hagw name
+		tunnel.VpcName1 += "-hagw"
+		tunnel.VpcName2 += "-hagw"
+		err := client.DeleteTunnel(tunnel)
+		if err != nil {
+			return fmt.Errorf("Failed to delete Aviatrix HA gateway: %s", err)
+		}
 
+	}
+	tunnel.VpcName1 = d.Get("vpc_name1").(string)
+	tunnel.VpcName2 = d.Get("vpc_name2").(string)
 	err := client.DeleteTunnel(tunnel)
 	if err != nil {
 		return fmt.Errorf("Failed to delete Aviatrix Tunnel: %s", err)
