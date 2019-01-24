936c647d29677903c665d7ea95dd0f2997cf5963
jalvarezferr <45765290+jalvarezferr@users.noreply.github.com>
2019-01-24 20:16:43 +0100

    Added import to gateway resource (#127)
    * Added import to gateway resource
    
    * Added import capacity to gateway resources
    
    
    Id format for import commands is <gateway name>@<account name>
    
    Example:
    
    terraform import aviatrix_gateway.my-gw-name my-gw-name@MyAccountName
    
    * Removed timeout configuration
    
    * Fixed how elb_name is deduced from elb_dns_name
    
    In cases where ELB is not autogenerated by Aviatrix, it is not valid to
    get the 31 first characters to get elb_name from elb_dns_name.
    No it is obtained removing what's after the first . (DNS zone name), and
    then the last section starting with a - in the hostname, which is
    introduced with a random value by AWs.

--- aviatrix/resource_gateway.go
+++ aviatrix/resource_gateway.go
@@ -3,6 +3,7 @@ package aviatrix
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/AviatrixSystems/go-aviatrix/goaviatrix"
 	"github.com/hashicorp/terraform/helper/schema"
@@ -14,6 +15,9 @@ func resourceAviatrixGateway() *schema.Resource {
 		Read:   resourceAviatrixGatewayRead,
 		Update: resourceAviatrixGatewayUpdate,
 		Delete: resourceAviatrixGatewayDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
 
 		Schema: map[string]*schema.Schema{
 			"cloud_type": {
@@ -281,11 +285,32 @@ func resourceAviatrixGatewayCreate(d *schema.ResourceData, meta interface{}) err
 		}
 	}
 	d.SetId(gateway.GwName)
+
 	return resourceAviatrixGatewayRead(d, meta)
 }
 
 func resourceAviatrixGatewayRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*goaviatrix.Client)
+	
+	gwname := d.Get("gw_name").(string)
+	// If it is an import only Id is set
+	if gwname == "" {
+		id := d.Id()
+		log.Printf("[DEBUG] Looks like an import, no gateway name received. Import Id is %s",id)
+		if strings.Contains(id,"@") {
+		    substr := strings.Split(id,"@")
+		    account_name := substr[1]
+		    gateway_name := substr[0]
+		    log.Printf("[INFO] Importing %s gateway in %s account",gateway_name,account_name)
+		    d.Set("account_name",account_name)
+		    d.Set("gw_name",gateway_name)
+		    // Terraform must locate a resource declared with the same Id returned
+		    d.SetId(gateway_name)
+		} else {
+			return fmt.Errorf("Id must be in the following format: <gateway name>@<account name>")
+		}
+	} 
+	
 	gateway := &goaviatrix.Gateway{
 		AccountName: d.Get("account_name").(string),
 		GwName:      d.Get("gw_name").(string),
@@ -303,12 +328,76 @@ func resourceAviatrixGatewayRead(d *schema.ResourceData, meta interface{}) error
 	}
 	log.Printf("[TRACE] reading gateway %s: %#v", d.Get("gw_name").(string), gw)
 	if gw != nil {
-		d.Set("vpc_size", gw.GwSize)
+
+		d.Set("cloud_type",gw.CloudType)
+		d.Set("account_name",gw.AccountName)
+		d.Set("gw_name",gw.GwName)
+		d.Set("vpc_id",strings.Split(gw.VpcID,"~~")[0])
+		d.Set("vpc_reg",gw.VpcRegion)
+		if gw.VpcNet != "" { d.Set("vpc_net",gw.VpcNet) }
+		if gw.EnableNat != ""  { d.Set("enable_nat",gw.EnableNat) }
+		if gw.DnsServer != "" { d.Set("dns_server",gw.DnsServer) }
+		if gw.VpnStatus != "" { d.Set("vpn_access",gw.VpnStatus) }
+		if gw.VpnCidr != "" { d.Set("cidr",gw.VpnCidr) }
+		if gw.ElbState == "enabled" {
+		    d.Set("enable_elb","yes")
+		    elb_name := d.Get("elb_name")
+		    // Versions prior to 4.0 won't return elb_name, so deduce it from elb_dns_name
+		    if elb_name == "" {
+		    	elb_dns_name := gw.ElbDNSName
+		    	log.Printf("[INFO] Controllers prior to 4.0 do not return elb_name. Deducing from elb_dns_name")
+		    	if elb_dns_name != "" {
+		    		hostname := strings.Split(elb_dns_name,".")[0]
+		    		// AWS adds - followed by a random string after the name given to the ELB
+		    		parts := strings.Split(hostname,"-")
+		    		// Remove random string added by AWS 
+		    		parts[len(parts)-1] = ""
+		    		parts = parts[:len(parts)-1]
+		    		// Join again using - in case it was used in the ELB Name
+		    		elb_name = strings.Join(parts,"-")
+		    	} else {
+		    		return fmt.Errorf("Neither elb_name or elb_dns_name returned by the API in an ELB enabled gateway")
+		    	}
+		    }
+		    d.Set("elb_name", elb_name)
+		} else {
+		    d.Set("enable_elb","no")
+		}
+		if gw.SplitTunnel != "" { d.Set("split_tunnel",gw.SplitTunnel) }
+		if gw.OtpMode != "" { d.Set("otp_mode",gw.OtpMode) }
+		if gw.SamlEnabled != "" { d.Set("saml_enabled",gw.SamlEnabled) }
+		if gw.OktaToken != "" { d.Set("okta_token",gw.OktaToken) }
+		if gw.OktaURL != "" { d.Set("okta_url",gw.OktaURL) }
+		if gw.OktaUsernameSuffix != "" { d.Set("okta_username_suffix",gw.OktaUsernameSuffix) }
+		if gw.DuoIntegrationKey != "" { d.Set("duo_integration_key",gw.DuoIntegrationKey) }
+		if gw.DuoSecretKey != "" { d.Set("duo_secret_key",gw.DuoSecretKey) }
+		if gw.DuoAPIHostname != "" { d.Set("duo_api_hostname",gw.DuoAPIHostname) }
+		if gw.DuoPushMode != "" { d.Set("duo_push_mode",gw.DuoPushMode) }
+		if gw.EnableLdap != "" { d.Set("enable_ldap",gw.EnableLdap) }
+		if gw.LdapServer != "" { d.Set("ldap_server",gw.LdapServer) }
+		if gw.LdapBindDn!= "" { d.Set("ldap_bind_dn",gw.LdapBindDn) }
+		if gw.LdapPassword != "" { d.Set("ldap_password",gw.LdapPassword) }
+		if gw.LdapBaseDn != "" { d.Set("ldap_base_dn",gw.LdapBaseDn) }
+		if gw.LdapUserAttr != "" { d.Set("ldap_username_attribute",gw.LdapUserAttr) }
+		if gw.HASubnet != "" { d.Set("ha_subnet",gw.HASubnet) }
+		if gw.PeeringHASubnet != "" { d.Set("public_subnet",gw.PeeringHASubnet) }
+		if gw.NewZone != "" { d.Set("zone",gw.NewZone) }
+		if gw.SingleAZ != "" { d.Set("single_az_ha",gw.SingleAZ) }
+	//	d.Set("allocate_new_eip",gw.AllocateNewEip)
+		if gw.Eip != "" { d.Set("eip",gw.Eip) }
+
+        // Though go_aviatrix Gateway struct declares VpcSize as only used on gateway creation
+        // it is the attribute receiving the instance size of an existing gateway instead of
+        // GwSize. (at least in v3.5)
+        if gw.GwSize != "" {
+        	d.Set("vpc_size",gw.GwSize)
+        } else {
+		   if gw.VpcSize != "" { d.Set("vpc_size", gw.VpcSize) }
+        }   
 		d.Set("public_ip", gw.PublicIP)
 		d.Set("cloud_instance_id", gw.CloudnGatewayInstID)
 		d.Set("public_dns_server", gw.PublicDnsServer)
 		d.Set("security_group_id", gw.GwSecurityGroupID)
-		d.Set("elb_name", gw.ElbName)
 
 		if publicSubnet := d.Get("public_subnet").(string); publicSubnet != "" {
 			gateway.GwName += "-hagw"
