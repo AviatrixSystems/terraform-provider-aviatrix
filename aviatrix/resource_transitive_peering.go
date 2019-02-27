917939637cdabc277e70fe8e8b53f03daa10408b
Zhiqiang Jin <zjin82@gmail.com>
2019-02-27 09:53:08 -0800

    Updated transitive_peering resoure to support import feature

--- aviatrix/resource_transitive_peering.go
+++ aviatrix/resource_transitive_peering.go
@@ -3,6 +3,7 @@ package aviatrix
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/AviatrixSystems/go-aviatrix/goaviatrix"
 	"github.com/hashicorp/terraform/helper/schema"
@@ -14,6 +15,9 @@ func resourceTransPeer() *schema.Resource {
 		Read:   resourceTransPeerRead,
 		Update: resourceTransPeerUpdate,
 		Delete: resourceTransPeerDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
 
 		Schema: map[string]*schema.Schema{
 			"source": {
@@ -46,13 +50,28 @@ func resourceTransPeerCreate(d *schema.ResourceData, meta interface{}) error {
 	if err != nil {
 		return fmt.Errorf("failed to create Aviatrix Transitive peering: %s", err)
 	}
-	d.SetId(transPeer.Source + transPeer.Nexthop + transPeer.ReachableCidr)
+	d.SetId(transPeer.Source + "~" + transPeer.Nexthop + "~" + transPeer.ReachableCidr)
 	//return nil
 	return resourceTransPeerRead(d, meta)
 }
 
 func resourceTransPeerRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*goaviatrix.Client)
+
+	sourceGw := d.Get("source").(string)
+	nestHopGw := d.Get("nexthop").(string)
+	reachableCIDR := d.Get("reachable_cidr").(string)
+
+	if sourceGw == "" || nestHopGw == "" || reachableCIDR == "" {
+		id := d.Id()
+		log.Printf("[DEBUG] Looks like an import, no transit gateway names or reachable cidr received. "+
+			"Import Id is %s", id)
+		d.Set("source", strings.Split(id, "~")[0])
+		d.Set("nexthop", strings.Split(id, "~")[1])
+		d.Set("reachable_cidr", strings.Split(id, "~")[2])
+		d.SetId(id)
+	}
+
 	transPeer := &goaviatrix.TransPeer{
 		Source:        d.Get("source").(string),
 		Nexthop:       d.Get("nexthop").(string),
