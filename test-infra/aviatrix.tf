c0ac7ecdbf5b2ec1c491cdb2739e3d91afee9706
Arvind <arvind@aviatrix.com>
2019-06-27 21:02:58 -0700

    Correct test infra (#513)
    * Correct test infra
    
    * conform to 0.12 as well

--- test-infra/aviatrix.tf
+++ test-infra/aviatrix.tf
@@ -7,7 +7,7 @@ data "aws_caller_identity" "current" {}
 # }
 
 module "aviatrix_controller_vpc" {
-  source               = "aws"
+  source               = "./aws"
   aws_vpc_cidr         = "${var.aws_vpc_cidr2}"
   aws_vpc_subnet       = "${var.aws_vpc_subnet2}"
   aws_region           = "${var.aws_region2}"
@@ -38,37 +38,34 @@ output "controller_private_ip" {
     value = "${module.aviatrix-controller-build.private_ip}"
 }
 
-output "controller_public_ip" {
-    value = "${module.aviatrix-controller-build.public_ip}"
-}
 module "aviatrix_gcp_vpc1" {
-  source               = "gcp"
+  source               = "./gcp"
   gcp_project_id       = "${var.gcp_project_id1}"
   gcp_region           = "${var.gcp_region1}"
   gcp_vpc_cidr         = "${var.gcp_vpc_cidr1}"
 }
 module "aviatrix_arm_vpc1" {
-  source               = "arm"
+  source               = "./arm"
   name                 = "vpc1"
   azure_region         = "${var.azure_region1}"
   azure_vpc_cidr       = "${var.azure_vpc_cidr1}"
   azure_vpc_subnet     = "${var.azure_vpc_subnet1}"
 }
 module "aviatrix_arm_vpc2" {
-  source               = "arm"
+  source               = "./arm"
   name                 = "vpc2"
   azure_region         = "${var.azure_region2}"
   azure_vpc_cidr       = "${var.azure_vpc_cidr2}"
   azure_vpc_subnet     = "${var.azure_vpc_subnet2}"
 }
 module "aviatrix_aws_vpc1" {
-  source               = "aws"
+  source               = "./aws"
   aws_vpc_cidr         = "${var.aws_vpc_cidr1}"
   aws_vpc_subnet       = "${var.aws_vpc_subnet1}"
   aws_region           = "${var.aws_region1}"
 }
 module "aviatrix_aws_vpc2" {
-  source               = "aws"
+  source               = "./aws"
   aws_vpc_cidr         = "${var.aws_vpc_cidr2}"
   aws_vpc_subnet       = "${var.aws_vpc_subnet2}"
   aws_region           = "${var.aws_region2}"
@@ -79,44 +76,4 @@ resource "aws_vpn_gateway" "vgw" {
     Name = "aviatrix-vgw"
   }
 }
-output "AWS_BGP_VGW_ID" {
-  value = "${aws_vpn_gateway.vgw.id}"
-} 
-output "GCP_VPC_ID" {
-  value = "${module.aviatrix_gcp_vpc1.vpc_id}"
-} 
-output "GCP_SUBNET" {
-  value = "${module.aviatrix_gcp_vpc1.subnet}"
-} 
-output "ARM_REGION" {
-  value = "${var.azure_region1}"
-} 
-output "ARM_VNET_ID" {
-  value = "${module.aviatrix_arm_vpc1.vnet}:${module.aviatrix_arm_vpc1.group}"
-}
-output "ARM_SUBNET" {
-  value = "${module.aviatrix_arm_vpc1.subnet}"
-}
-output "ARM_GW_SIZE" {
-  value = "${module.aviatrix_arm_vpc1.subnet}"
-}
-output "ARM_VNET_ID2" {
-  value = "${module.aviatrix_arm_vpc2.vnet}:${module.aviatrix_arm_vpc2.group}"
-} 
-output "ARM_REGION2" {
-  value = "${var.azure_region2}"
-} 
-output "AWS_VPC_ID" {
-  value = "${module.aviatrix_aws_vpc1.vpc}"
-} 
-output "AWS_VPC_NET" {
-  value = "${module.aviatrix_aws_vpc1.subnet}"
-} 
-output "AWS_VPC_ID2" {
-  value = "${module.aviatrix_aws_vpc2.vpc}"
-} 
-output "AWS_VPC_NET2" {
-  value = "${module.aviatrix_aws_vpc2.subnet}"
-} 
-
 
--- test-infra/outputs.tf
+++ test-infra/outputs.tf
@@ -1,21 +1,102 @@
-#output "aws_vpc_id" {
-#  value = "${aws_vpc.vpc.*.id}"
-#}
-#output "aws_vpc-subnet" {
-#  value = "${aws_subnet.vpc-public-ha.*.cidr_block}"
-#}
-#output "arm_vnet" {
-#  value = "${azurerm_virtual_network.hashicorp.*.name}"
-#}
-#output "arm_resource_group" {
-#  value = "${azurerm_virtual_network.hashicorp.*.resource_group_name}"
-#}
-#output "arm_vpc-subnet" {
-#  value = "${azurerm_subnet.hashicorp.*.address_prefix}"
-#}
-#output "gcp_vpc_id" {
-#  value = "${google_compute_network.vpc.*.id}"
-#}
-#output "gcp_vpc-subnet" {
-#  value = "${google_compute_subnetwork.subnet.*.ip_cidr_range}"
-#}
+output "AVIATRIX_CONTROLLER_IP" {
+    value = "${module.aviatrix-controller-build.public_ip}"
+}
+
+output "AVIATRIX_USERNAME" {
+    value = "admin"
+}
+
+output "AVIATRIX_PASSWORD" {
+    value = "${var.admin_password}"
+}
+
+output "AWS_ACCOUNT_NUMBER" {
+    value = "${data.aws_caller_identity.current.account_id}"
+}
+
+output "AWS_ACCESS_KEY" {
+    value = "${var.aws_access_key}"
+}
+
+output "AWS_SECRET_KEY" {
+    value = "${var.aws_secret_key}"
+}
+
+
+output "ARM_SUBSCRIPTION_ID" {
+    value = "${var.azure_subscription_id}"
+}
+
+output "ARM_DIRECTORY_ID" {
+    value = "${var.azure_tenant_id}"
+}
+
+output "ARM_APPLICATION_ID" {
+    value = "${var.azure_client_id}"
+}
+
+output "ARM_APPLICATION_KEY"{
+    value = "${var.azure_client_secret}"
+}
+
+output "GCP_ID" {
+    value = "${var.gcp_project_id1}"
+}
+
+output "GCP_CREDENTIALS_FILEPATH"{
+    value = "${var.gcp_credentials_file_path}"
+}
+
+
+output "AWS_BGP_VGW_ID" {
+  value = "${aws_vpn_gateway.vgw.id}"
+} 
+
+output "GCP_VPC_ID" {
+  value = "${module.aviatrix_gcp_vpc1.vpc_id}"
+} 
+
+output "GCP_SUBNET" {
+  value = "${module.aviatrix_gcp_vpc1.subnet}"
+} 
+
+output "ARM_REGION" {
+  value = "${var.azure_region1}"
+} 
+
+output "ARM_VNET_ID" {
+  value = "${module.aviatrix_arm_vpc1.vnet}:${module.aviatrix_arm_vpc1.group}"
+}
+
+output "ARM_SUBNET" {
+  value = "${module.aviatrix_arm_vpc1.subnet}"
+}
+
+output "ARM_GW_SIZE" {
+  value = "${module.aviatrix_arm_vpc1.subnet}"
+}
+
+output "ARM_VNET_ID2" {
+  value = "${module.aviatrix_arm_vpc2.vnet}:${module.aviatrix_arm_vpc2.group}"
+} 
+
+output "ARM_REGION2" {
+  value = "${var.azure_region2}"
+} 
+
+output "AWS_VPC_ID" {
+  value = "${module.aviatrix_aws_vpc1.vpc}"
+} 
+
+output "AWS_VPC_NET" {
+  value = "${module.aviatrix_aws_vpc1.subnet}"
+} 
+
+output "AWS_VPC_ID2" {
+  value = "${module.aviatrix_aws_vpc2.vpc}"
+} 
+
+output "AWS_VPC_NET2" {
+  value = "${module.aviatrix_aws_vpc2.subnet}"
+} 
+
