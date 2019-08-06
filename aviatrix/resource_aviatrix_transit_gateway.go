d815fe2f21ec56fe815a720664322c466c4f3a7e
ZHIQIANG JIN <44539929+zhiqiangjin@users.noreply.github.com>
2019-08-06 16:04:31 -0700

    Fix eip value set issue for spoke_gateway and transit_gateway (#625)

--- aviatrix/resource_aviatrix_transit_gateway.go
+++ aviatrix/resource_aviatrix_transit_gateway.go
@@ -390,13 +390,12 @@ func resourceAviatrixTransitGatewayRead(d *schema.ResourceData, meta interface{}
 				d.Set("allocate_new_eip", true)
 			} else {
 				d.Set("allocate_new_eip", false)
+				d.Set("eip", gw.PublicIP)
 			}
 		} else if gw.CloudType == 8 {
 			d.Set("vpc_id", gw.VpcID)
 			d.Set("allocate_new_eip", true)
 		}
-
-		d.Set("eip", gw.PublicIP)
 
 		d.Set("vpc_reg", gw.VpcRegion)
 		d.Set("gw_size", gw.GwSize)
