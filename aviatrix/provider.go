35459689a8704235abffd0f98395a4023c72e51e
ZHIQIANG JIN <44539929+zhiqiangjin@users.noreply.github.com>
2019-10-18 16:51:00 -0700

    Update supported_version to 5.2 (#118) (#120)

--- aviatrix/provider.go
+++ aviatrix/provider.go
@@ -8,7 +8,7 @@ import (
 	"github.com/hashicorp/terraform/terraform"
 )
 
-const supportedVersion = "5.1"
+const supportedVersion = "5.2"
 
 // Provider returns a schema.Provider for Aviatrix.
 func Provider() terraform.ResourceProvider {
