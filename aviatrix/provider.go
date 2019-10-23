b4afeceb92abdf13165325606225339592e58263
Zhiqiang Jin <zjin82@gmail.com>
2019-10-23 16:50:17 -0700

    Change supportedVersions to 5.2 only for future branch

--- aviatrix/provider.go
+++ aviatrix/provider.go
@@ -8,7 +8,7 @@ import (
 	"github.com/hashicorp/terraform/terraform"
 )
 
-var supportedVersions = []string{"5.1"}
+var supportedVersions = []string{"5.2"}
 
 // Provider returns a schema.Provider for Aviatrix.
 func Provider() terraform.ResourceProvider {
