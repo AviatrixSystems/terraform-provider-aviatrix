fa68b921030d2efe8860aa62e8d1341ab883b088
Zhiqiang Jin <zjin82@gmail.com>
2019-12-02 12:25:46 -0800

    Update test-infra to name of gcp-vpc as gcp_vpc_id

--- test-infra/gcp/outputs.tf
+++ test-infra/gcp/outputs.tf
@@ -1,5 +1,5 @@
 output "vpc_id" {
-   value = google_compute_network.vpc.id
+   value = google_compute_network.vpc.name
 } 
 output "subnet" {
    value = google_compute_subnetwork.subnet.ip_cidr_range
