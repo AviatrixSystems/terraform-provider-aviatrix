24c712773c92e79e8d192796d46c9d98557cbe93
Zhiqiang Jin <zjin82@gmail.com>
2020-02-24 16:01:43 -0800

    (0013061) Update aviatrix_transit_firenet_policy resource

--- goaviatrix/transit_firenet_policy.go
+++ goaviatrix/transit_firenet_policy.go
@@ -9,23 +9,23 @@ import (
 	"strings"
 )
 
-type TransitFireNetInspection struct {
+type TransitFireNetPolicy struct {
 	TransitFireNetGatewayName string `form:"gateway_1,omitempty" json:"gateway_1,omitempty"`
 	InspectedResourceName     string `form:"gateway_2,omitempty" json:"gateway_2,omitempty"`
 }
 
-type TransitFireNetInspectionAPIResp struct {
-	Return  bool                           `json:"return"`
-	Results []TransitFireNetInspectionEdit `json:"results"`
-	Reason  string                         `json:"reason"`
+type TransitFireNetPolicyAPIResp struct {
+	Return  bool                       `json:"return"`
+	Results []TransitFireNetPolicyEdit `json:"results"`
+	Reason  string                     `json:"reason"`
 }
 
-type TransitFireNetInspectionEdit struct {
+type TransitFireNetPolicyEdit struct {
 	TransitFireNetGwName      string   `json:"gw_name,omitempty"`
 	InspectedResourceNameList []string `json:"inspected,omitempty"`
 }
 
-func (c *Client) CreateTransitFireNetInspection(transitFireNetInspection *TransitFireNetInspection) error {
+func (c *Client) CreateTransitFireNetPolicy(transitFireNetPolicy *TransitFireNetPolicy) error {
 	Url, err := url.Parse(c.baseURL)
 	if err != nil {
 		return errors.New(("url Parsing failed for 'CreateTransitFireNetInspection': ") + err.Error())
@@ -33,8 +33,8 @@ func (c *Client) CreateTransitFireNetInspection(transitFireNetInspection *Transi
 	CreateTransitFireNetInspection := url.Values{}
 	CreateTransitFireNetInspection.Add("CID", c.CID)
 	CreateTransitFireNetInspection.Add("action", "add_spoke_to_transit_firenet_inspection")
-	CreateTransitFireNetInspection.Add("firenet_gateway_name", transitFireNetInspection.TransitFireNetGatewayName)
-	CreateTransitFireNetInspection.Add("spoke_gateway_name", transitFireNetInspection.InspectedResourceName)
+	CreateTransitFireNetInspection.Add("firenet_gateway_name", transitFireNetPolicy.TransitFireNetGatewayName)
+	CreateTransitFireNetInspection.Add("spoke_gateway_name", transitFireNetPolicy.InspectedResourceName)
 	Url.RawQuery = CreateTransitFireNetInspection.Encode()
 	resp, err := c.Get(Url.String(), nil)
 	if err != nil {
@@ -54,10 +54,10 @@ func (c *Client) CreateTransitFireNetInspection(transitFireNetInspection *Transi
 	return nil
 }
 
-func (c *Client) GetTransitFireNetInspection(transitFireNetInspection *TransitFireNetInspection) error {
+func (c *Client) GetTransitFireNetPolicy(transitFireNetPolicy *TransitFireNetPolicy) error {
 	Url, err := url.Parse(c.baseURL)
 	if err != nil {
-		return errors.New(("url Parsing failed for GetTransitFireNetInspection: ") + err.Error())
+		return errors.New(("url Parsing failed for GetTransitFireNetPolicy: ") + err.Error())
 	}
 	listTransitFireNetSpokePolicies := url.Values{}
 	listTransitFireNetSpokePolicies.Add("CID", c.CID)
@@ -68,7 +68,7 @@ func (c *Client) GetTransitFireNetInspection(transitFireNetInspection *TransitFi
 	if err != nil {
 		return errors.New("HTTP Get list_transit_firenet_spoke_policies failed: " + err.Error())
 	}
-	var data TransitFireNetInspectionAPIResp
+	var data TransitFireNetPolicyAPIResp
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(resp.Body)
 	bodyString := buf.String()
@@ -81,16 +81,16 @@ func (c *Client) GetTransitFireNetInspection(transitFireNetInspection *TransitFi
 	}
 	if len(data.Results) == 0 {
 		log.Printf("Transit gateway peering with transit firenet gateway: %s and inspected resource name: %s not found",
-			transitFireNetInspection.TransitFireNetGatewayName, transitFireNetInspection.InspectedResourceName)
+			transitFireNetPolicy.TransitFireNetGatewayName, transitFireNetPolicy.InspectedResourceName)
 		return ErrNotFound
 	}
-	inspectionList := data.Results
-	for i := range inspectionList {
-		if inspectionList[i].TransitFireNetGwName != transitFireNetInspection.TransitFireNetGatewayName {
+	policyList := data.Results
+	for i := range policyList {
+		if policyList[i].TransitFireNetGwName != transitFireNetPolicy.TransitFireNetGatewayName {
 			continue
 		}
-		for j := range inspectionList[i].InspectedResourceNameList {
-			if inspectionList[i].InspectedResourceNameList[j] == transitFireNetInspection.InspectedResourceName {
+		for j := range policyList[i].InspectedResourceNameList {
+			if policyList[i].InspectedResourceNameList[j] == transitFireNetPolicy.InspectedResourceName {
 				return nil
 			}
 		}
@@ -98,16 +98,16 @@ func (c *Client) GetTransitFireNetInspection(transitFireNetInspection *TransitFi
 	return ErrNotFound
 }
 
-func (c *Client) DeleteTransitFireNetInspection(transitFireNetInspection *TransitFireNetInspection) error {
+func (c *Client) DeleteTransitFireNetPolicy(transitFireNetPolicy *TransitFireNetPolicy) error {
 	Url, err := url.Parse(c.baseURL)
 	if err != nil {
-		return errors.New(("url Parsing failed for 'DeleteTransitFireNetInspection': ") + err.Error())
+		return errors.New(("url Parsing failed for 'DeleteTransitFireNetPolicy': ") + err.Error())
 	}
 	CreateTransitFireNetInspection := url.Values{}
 	CreateTransitFireNetInspection.Add("CID", c.CID)
 	CreateTransitFireNetInspection.Add("action", "delete_spoke_from_transit_firenet_inspection")
-	CreateTransitFireNetInspection.Add("firenet_gateway_name", transitFireNetInspection.TransitFireNetGatewayName)
-	CreateTransitFireNetInspection.Add("spoke_gateway_name", transitFireNetInspection.InspectedResourceName)
+	CreateTransitFireNetInspection.Add("firenet_gateway_name", transitFireNetPolicy.TransitFireNetGatewayName)
+	CreateTransitFireNetInspection.Add("spoke_gateway_name", transitFireNetPolicy.InspectedResourceName)
 	Url.RawQuery = CreateTransitFireNetInspection.Encode()
 	resp, err := c.Get(Url.String(), nil)
 	if err != nil {
