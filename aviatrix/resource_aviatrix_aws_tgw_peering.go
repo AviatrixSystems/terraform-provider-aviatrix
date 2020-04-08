2483c5c2705e7c47a87b7de09a513f51d9bca438
Zhiqiang Jin <zjin82@gmail.com>
2020-04-08 16:14:41 -0700

    (0012496): Implement a new resource for AWS TGW inter region peering support

---
+++ aviatrix/resource_aviatrix_aws_tgw_peering.go
@@ -0,0 +1,105 @@
+package aviatrix
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/terraform-providers/terraform-provider-aviatrix/goaviatrix"
+)
+
+func resourceAviatrixAWSTgwPeering() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAviatrixAWSTgwPeeringCreate,
+		Read:   resourceAviatrixAWSTgwPeeringRead,
+		Delete: resourceAviatrixAWSTgwPeeringDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"tgw_name1": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "Name of the first AWS tgw to make a peer pair.",
+			},
+			"tgw_name2": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "Name of the second AWS tgw to make a peer pair.",
+			},
+		},
+	}
+}
+
+func resourceAviatrixAWSTgwPeeringCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*goaviatrix.Client)
+
+	awsTgwPeering := &goaviatrix.AwsTgwPeering{
+		TgwName1: d.Get("tgw_name1").(string),
+		TgwName2: d.Get("tgw_name2").(string),
+	}
+
+	log.Printf("[INFO] Creating Aviatrix AWS tgw peering: %#v", awsTgwPeering)
+
+	err := client.CreateAwsTgwPeering(awsTgwPeering)
+	if err != nil {
+		return fmt.Errorf("failed to create Aviatrix AWS tgw peering: %s", err)
+	}
+
+	d.SetId(awsTgwPeering.TgwName1 + "~" + awsTgwPeering.TgwName2)
+	return resourceAviatrixAWSTgwPeeringRead(d, meta)
+}
+
+func resourceAviatrixAWSTgwPeeringRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*goaviatrix.Client)
+
+	tgwName1 := d.Get("tgw_name1").(string)
+	tgwName2 := d.Get("tgw_name2").(string)
+
+	if tgwName1 == "" || tgwName2 == "" {
+		id := d.Id()
+		log.Printf("[DEBUG] Looks like an import. Import Id is %s", id)
+		d.Set("tgw_name1", strings.Split(id, "~")[0])
+		d.Set("tgw_name2", strings.Split(id, "~")[1])
+		d.SetId(id)
+	}
+
+	awsTgwPeering := &goaviatrix.AwsTgwPeering{
+		TgwName1: d.Get("tgw_name1").(string),
+		TgwName2: d.Get("tgw_name2").(string),
+	}
+
+	err := client.GetAwsTgwPeering(awsTgwPeering)
+	if err != nil {
+		if err == goaviatrix.ErrNotFound {
+			d.SetId("")
+			return nil
+		}
+		return fmt.Errorf("couldn't find Aviatrix AWS tgw peering: %s", err)
+	}
+
+	d.SetId(awsTgwPeering.TgwName1 + "~" + awsTgwPeering.TgwName2)
+	return nil
+}
+
+func resourceAviatrixAWSTgwPeeringDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*goaviatrix.Client)
+
+	awsTgwPeering := &goaviatrix.AwsTgwPeering{
+		TgwName1: d.Get("tgw_name1").(string),
+		TgwName2: d.Get("tgw_name2").(string),
+	}
+
+	log.Printf("[INFO] Deleting Aviatrix AWS tgw peering: %#v", awsTgwPeering)
+
+	err := client.DeleteAwsTgwPeering(awsTgwPeering)
+	if err != nil {
+		return fmt.Errorf("failed to delete Aviatrix AWS tgw peering: %s", err)
+	}
+
+	return nil
+}
---
+++ goaviatrix/aws_tgw_peering.go
@@ -0,0 +1,104 @@
+package goaviatrix
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/url"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type AwsTgwPeering struct {
+	Action   string `form:"action,omitempty"`
+	CID      string `form:"CID,omitempty"`
+	TgwName1 string `form:"tgw_name1,omitempty" json:"tgw_name1,omitempty"`
+	TgwName2 string `form:"tgw_name2,omitempty" json:"tgw_name2,omitempty"`
+}
+
+type AwsTgwPeeringAPIResp struct {
+	Return  bool     `json:"return"`
+	Results []string `json:"results"`
+	Reason  string   `json:"reason"`
+}
+
+func (c *Client) CreateAwsTgwPeering(awsTgwPeering *AwsTgwPeering) error {
+	awsTgwPeering.CID = c.CID
+	awsTgwPeering.Action = "add_tgw_peering"
+	resp, err := c.Post(c.baseURL, awsTgwPeering)
+	if err != nil {
+		return errors.New("HTTP Post 'add_tgw_peering' failed: " + err.Error())
+	}
+	var data APIResp
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	bodyString := buf.String()
+	bodyIoCopy := strings.NewReader(bodyString)
+	if err = json.NewDecoder(bodyIoCopy).Decode(&data); err != nil {
+		return errors.New("Json Decode 'add_tgw_peering' failed: " + err.Error() + "\n Body: " + bodyString)
+	}
+	if !data.Return {
+		return errors.New("Rest API 'add_tgw_peering' Post failed: " + data.Reason)
+	}
+	return nil
+}
+
+func (c *Client) GetAwsTgwPeering(awsTgwPeering *AwsTgwPeering) error {
+	Url, err := url.Parse(c.baseURL)
+	if err != nil {
+		return errors.New(("url Parsing failed for 'list_peered_tgw_names': ") + err.Error())
+	}
+	listPeeredTgwNames := url.Values{}
+	listPeeredTgwNames.Add("CID", c.CID)
+	listPeeredTgwNames.Add("action", "list_peered_tgw_names")
+	listPeeredTgwNames.Add("tgw_name", awsTgwPeering.TgwName1)
+	Url.RawQuery = listPeeredTgwNames.Encode()
+	resp, err := c.Get(Url.String(), nil)
+	if err != nil {
+		return errors.New("HTTP Get 'list_peered_tgw_names' failed: " + err.Error())
+	}
+	var data AwsTgwPeeringAPIResp
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	bodyString := buf.String()
+	bodyIoCopy := strings.NewReader(bodyString)
+	if err = json.NewDecoder(bodyIoCopy).Decode(&data); err != nil {
+		return errors.New("Json Decode 'list_peered_tgw_names' failed: " + err.Error() + "\n Body: " + bodyString)
+	}
+	if !data.Return {
+		return errors.New("Rest API 'list_peered_tgw_names' Get failed: " + data.Reason)
+	}
+	if len(data.Results) == 0 {
+		log.Errorf("Aws tgw peering with tgw: %s and tgw: %s not found", awsTgwPeering.TgwName1, awsTgwPeering.TgwName2)
+		return ErrNotFound
+	}
+	peeringList := data.Results
+	for i := range peeringList {
+		if peeringList[i] == awsTgwPeering.TgwName2 {
+			return nil
+		}
+	}
+	return ErrNotFound
+}
+
+func (c *Client) DeleteAwsTgwPeering(awsTgwPeering *AwsTgwPeering) error {
+	awsTgwPeering.CID = c.CID
+	awsTgwPeering.Action = "delete_tgw_peering"
+	resp, err := c.Post(c.baseURL, awsTgwPeering)
+	if err != nil {
+		return errors.New("HTTP Post 'delete_tgw_peering' failed: " + err.Error())
+	}
+	var data APIResp
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	bodyString := buf.String()
+	bodyIoCopy := strings.NewReader(bodyString)
+	if err = json.NewDecoder(bodyIoCopy).Decode(&data); err != nil {
+		return errors.New("Json Decode 'delete_tgw_peering' failed: " + err.Error() + "\n Body: " + bodyString)
+	}
+	if !data.Return {
+		return errors.New("Rest API 'delete_tgw_peering' Post failed: " + data.Reason)
+	}
+	return nil
+}
