d1b63a3a7ee21e5bb076bb6e33285d42bb1bfc5d
Zhiqiang Jin <zjin82@gmail.com>
2020-04-27 20:54:34 -0700

    (0014137) Update data source gateway to be latest

--- aviatrix/data_source_aviatrix_gateway.go
+++ aviatrix/data_source_aviatrix_gateway.go
@@ -18,35 +18,270 @@ func dataSourceAviatrixGateway() *schema.Resource {
 				Required:    true,
 				Description: "Gateway name. This can be used for getting gateway.",
 			},
-			"account_name": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Description: "Account name. This can be used for logging in to CloudN console or UserConnect controller.",
-			},
 			"cloud_type": {
 				Type:        schema.TypeInt,
 				Computed:    true,
 				Description: "Type of cloud service provider.",
 			},
+			"account_name": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Account name. This account will be used to launch Aviatrix gateway.",
+			},
 			"vpc_id": {
 				Type:        schema.TypeString,
 				Computed:    true,
-				Description: "AWS VPC ID.",
+				Description: "ID of legacy VPC/Vnet to be connected.",
 			},
 			"vpc_reg": {
 				Type:        schema.TypeString,
 				Computed:    true,
-				Description: "AWS VPC Region.",
+				Description: "Region where gateway is launched.",
 			},
-			"vpc_size": {
+			"gw_size": {
 				Type:        schema.TypeString,
 				Computed:    true,
-				Description: "Instance type.",
+				Description: "Size of Gateway Instance.",
+			},
+			"subnet": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "A VPC Network address range selected from one of the available network ranges.",
+			},
+			"insane_mode_az": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "AZ of subnet being created for Insane Mode Gateway. Required if insane_mode is set.",
+			},
+			"single_ip_snat": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Enable Source NAT for this container.",
+			},
+			"vpn_access": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Enable user access through VPN to this container.",
+			},
+			"vpn_cidr": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "VPN CIDR block for the container.",
+			},
+			"enable_elb": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Specify whether to enable ELB or not.",
+			},
+			"elb_name": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "A name for the ELB that is created.",
+			},
+			"vpn_protocol": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Elb protocol for VPN gateway with elb enabled. Only supports AWS provider. Valid values: 'TCP', 'UDP'. If not specified, 'TCP'' will be used.",
+			},
+			"split_tunnel": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Specify split tunnel mode.",
+			},
+			"max_vpn_conn": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Maximum connection of VPN access.",
+			},
+			"name_servers": {
+				Type:     schema.TypeString,
+				Computed: true,
+				Description: "A list of DNS servers used to resolve domain names by " +
+					"a connected VPN user when Split Tunnel Mode is enabled.",
+			},
+			"search_domains": {
+				Type:     schema.TypeString,
+				Computed: true,
+				Description: "A list of domain names that will use the NameServer " +
+					"when a specific name is not in the destination when Split Tunnel Mode is enabled.",
+			},
+			"additional_cidrs": {
+				Type:     schema.TypeString,
+				Computed: true,
+				Description: "A list of destination CIDR ranges that will also go through the VPN tunnel " +
+					"when Split Tunnel Mode is enabled.",
+			},
+			"otp_mode": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Two step authentication mode.",
+			},
+			"saml_enabled": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "This field indicates whether to enable SAML or not.",
+			},
+			"enable_vpn_nat": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "This field indicates whether to enable VPN NAT or not. Only supported for VPN gateway. Valid values: true, false. Default value: true.",
+			},
+			"okta_url": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "URL for Okta auth mode.",
+			},
+			"okta_username_suffix": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Username suffix for Okta auth mode.",
+			},
+			"duo_integration_key": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Integration key for DUO auth mode.",
+			},
+			"duo_api_hostname": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "API hostname for DUO auth mode.",
+			},
+			"duo_push_mode": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Push mode for DUO auth.",
+			},
+			"enable_ldap": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Specify whether to enable LDAP or not. Supported values: 'yes' and 'no'.",
+			},
+			"ldap_server": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "LDAP server address. Required: Yes if enable_ldap is 'yes'.",
+			},
+			"ldap_bind_dn": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "LDAP bind DN. Required: Yes if enable_ldap is 'yes'.",
+			},
+			"ldap_base_dn": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "LDAP base DN. Required: Yes if enable_ldap is 'yes'.",
+			},
+			"ldap_username_attribute": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "LDAP user attribute. Required: Yes if enable_ldap is 'yes'.",
+			},
+			"peering_ha_subnet": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Public Subnet Information while creating Peering HA Gateway, only subnet is accepted. Required to create peering ha gateway if cloud_type = 1 or 8 (AWS or AZURE)",
+			},
+			"peering_ha_zone": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Zone information for creating Peering HA Gateway. Required to create peering ha gateway if cloud_type = 4 (gcp)",
+			},
+			"peering_ha_insane_mode_az": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "AZ of subnet being created for Insane Mode Peering HA Gateway. Required if insane_mode is set.",
+			},
+			"peering_ha_public_ip": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Public IP address that you want assigned to the HA peering instance.",
+			},
+			"peering_ha_gw_size": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Peering HA Gateway Size.",
+			},
+			"single_az_ha": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Set to true if this feature is desired.",
+			},
+			"allocate_new_eip": {
+				Type:     schema.TypeBool,
+				Computed: true,
+				Description: "When value is false, reuse an idle address in Elastic IP pool for this gateway. " +
+					"Otherwise, allocate a new Elastic IP and use it for this gateway.",
+			},
+			"tag_list": {
+				Type:        schema.TypeList,
+				Elem:        &schema.Schema{Type: schema.TypeString},
+				Computed:    true,
+				Description: "Instance tag of cloud provider.",
+			},
+			"insane_mode": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Enable Insane Mode for Gateway. Valid values: true, false.",
+			},
+			"enable_vpc_dns_server": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Enable vpc_dns_server for Gateway. Only supports AWS. Valid values: true, false.",
+			},
+			"enable_designated_gateway": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Enable 'designated_gateway' feature for Gateway. Only supports AWS. Valid values: true, false.",
+			},
+			"additional_cidrs_designated_gateway": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "A list of CIDR ranges separated by comma to configure when 'designated_gateway' feature is enabled.",
+			},
+			"enable_encrypt_volume": {
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Enable encrypt gateway EBS volume. Only supported for AWS provider. Valid values: true, false. Default value: false.",
+			},
+			"elb_dns_name": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "ELB DNS Name.",
 			},
 			"public_ip": {
 				Type:        schema.TypeString,
 				Computed:    true,
 				Description: "Public IP address of the Gateway created.",
+			},
+			"security_group_id": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Security group used for the gateway.",
+			},
+			"public_dns_server": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "NS server used by the gateway.",
+			},
+			"cloud_instance_id": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Instance ID of the gateway.",
+			},
+			"private_ip": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Private IP address of the Gateway created.",
+			},
+			"peering_ha_cloud_instance_id": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Instance ID of the peering HA gateway.",
+			},
+			"peering_ha_gw_name": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Aviatrix gateway unique name of HA gateway.",
 			},
 		},
 	}
@@ -68,18 +303,241 @@ func dataSourceAviatrixGatewayRead(d *schema.ResourceData, meta interface{}) err
 		return fmt.Errorf("couldn't find Aviatrix Gateway: %s", err)
 	}
 	if gw != nil {
-		index := strings.Index(gw.VpcID, "~~")
-		if index > 0 {
-			gw.VpcID = gw.VpcID[:index]
-		}
 		d.Set("cloud_type", gw.CloudType)
 		d.Set("account_name", gw.AccountName)
 		d.Set("gw_name", gw.GwName)
-		d.Set("vpc_id", gw.VpcID)
-		d.Set("vpc_reg", gw.VpcRegion)
-		d.Set("vpc_size", gw.GwSize)
-		d.Set("vpc_net", gw.VpcNet)
+
+		if gw.CloudType == 1 || gw.CloudType == 16 || gw.CloudType == 256 {
+			d.Set("vpc_id", strings.Split(gw.VpcID, "~~")[0])
+			d.Set("vpc_reg", gw.VpcRegion)
+		} else if gw.CloudType == 4 {
+			d.Set("vpc_id", strings.Split(gw.VpcID, "~-~")[0])
+			d.Set("vpc_reg", gw.GatewayZone)
+		} else if gw.CloudType == 8 {
+			d.Set("vpc_id", gw.VpcID)
+			d.Set("vpc_reg", gw.VpcRegion)
+		}
+
+		d.Set("subnet", gw.VpcNet)
+
+		if gw.EnableNat == "yes" {
+			if gw.SnatMode == "primary" {
+				d.Set("single_ip_snat", true)
+			} else {
+				d.Set("single_ip_snat", false)
+			}
+		} else {
+			d.Set("single_ip_snat", false)
+		}
+
+		if gw.CloudType == 1 || gw.CloudType == 256 {
+			if gw.AllocateNewEipRead {
+				d.Set("allocate_new_eip", true)
+			} else {
+				d.Set("allocate_new_eip", false)
+			}
+		} else if gw.CloudType == 4 || gw.CloudType == 8 || gw.CloudType == 16 {
+			d.Set("allocate_new_eip", true)
+		}
+
+		if gw.EnableDesignatedGateway == "Yes" || gw.EnableDesignatedGateway == "yes" {
+			d.Set("enable_designated_gateway", true)
+			d.Set("additional_cidrs_designated_gateway", gw.AdditionalCidrsDesignatedGw)
+		} else {
+			d.Set("enable_designated_gateway", false)
+			d.Set("additional_cidrs_designated_gateway", "")
+		}
+
+		if gw.EnableLdapRead {
+			d.Set("enable_ldap", true)
+		} else {
+			d.Set("enable_ldap", false)
+		}
+
+		gwDetail, err := client.GetGatewayDetail(gateway)
+		if err != nil {
+			return fmt.Errorf("couldn't get Detail info for VPN gateway: %s due to: %s", gateway.GwName, err)
+		}
+		if gw.VpnStatus != "" {
+			if gw.VpnStatus == "disabled" {
+				d.Set("vpn_access", false)
+				d.Set("enable_vpn_nat", true)
+				d.Set("vpn_protocol", "")
+			} else if gw.VpnStatus == "enabled" {
+				d.Set("vpn_access", true)
+				gateway.VpcID = d.Get("vpc_id").(string)
+				if gwDetail.VpnNat {
+					d.Set("enable_vpn_nat", true)
+				} else {
+					d.Set("enable_vpn_nat", false)
+				}
+
+				if gw.ElbState == "enabled" {
+					if gwDetail.Elb.VpnProtocol == "udp" || gwDetail.Elb.VpnProtocol == "UDP" {
+						d.Set("vpn_protocol", "UDP")
+					} else {
+						d.Set("vpn_protocol", "TCP")
+					}
+				} else {
+					d.Set("vpn_protocol", "")
+				}
+			}
+		}
+
+		vpnAccess := d.Get("vpn_access").(bool)
+		if !vpnAccess {
+			d.Set("split_tunnel", true)
+			d.Set("max_vpn_conn", "")
+		} else {
+			if gw.SplitTunnel == "yes" {
+				d.Set("split_tunnel", true)
+			} else {
+				d.Set("split_tunnel", false)
+			}
+
+			d.Set("max_vpn_conn", gw.MaxConn)
+		}
+
+		d.Set("vpn_cidr", gw.VpnCidr)
+
+		if gw.ElbState == "enabled" {
+			d.Set("enable_elb", true)
+			d.Set("elb_name", gw.ElbName)
+			d.Set("elb_dns_name", gw.ElbDNSName)
+		} else {
+			d.Set("enable_elb", false)
+			d.Set("elb_name", "")
+		}
+
+		if gw.SamlEnabled == "yes" {
+			d.Set("saml_enabled", true)
+		} else {
+			d.Set("saml_enabled", false)
+		}
+
+		if gw.AuthMethod == "duo_auth" || gw.AuthMethod == "duo_auth+LDAP" {
+			d.Set("otp_mode", "2")
+		} else if gw.AuthMethod == "okta_auth" {
+			d.Set("otp_mode", "3")
+		} else {
+			d.Set("otp_mode", "")
+		}
+
+		d.Set("okta_url", gw.OktaURL)
+		d.Set("okta_username_suffix", gw.OktaUsernameSuffix)
+		d.Set("duo_integration_key", gw.DuoIntegrationKey)
+		d.Set("duo_api_hostname", gw.DuoAPIHostname)
+		d.Set("duo_push_mode", gw.DuoPushMode)
+		d.Set("ldap_server", gw.LdapServer)
+		d.Set("ldap_bind_dn", gw.LdapBindDn)
+		d.Set("ldap_base_dn", gw.LdapBaseDn)
+		d.Set("ldap_username_attribute", gw.LdapUserAttr)
+
+		if gw.NewZone != "" {
+			d.Set("zone", gw.NewZone)
+		}
+
+		if gw.SingleAZ != "" {
+			if gw.SingleAZ == "yes" {
+				d.Set("single_az_ha", true)
+			} else {
+				d.Set("single_az_ha", false)
+			}
+		}
+		d.Set("enable_encrypt_volume", gw.EnableEncryptVolume)
 		d.Set("public_ip", gw.PublicIP)
+
+		if gw.GwSize != "" {
+			d.Set("gw_size", gw.GwSize)
+		} else {
+			if gw.VpcSize != "" {
+				d.Set("gw_size", gw.VpcSize)
+			}
+		}
+
+		d.Set("public_ip", gw.PublicIP)
+		d.Set("cloud_instance_id", gw.CloudnGatewayInstID)
+		d.Set("public_dns_server", gw.PublicDnsServer)
+		d.Set("security_group_id", gw.GwSecurityGroupID)
+		d.Set("private_ip", gw.PrivateIP)
+
+		if gw.InsaneMode == "yes" {
+			d.Set("insane_mode", true)
+			if gw.CloudType == 1 || gw.CloudType == 256 {
+				d.Set("insane_mode_az", gw.GatewayZone)
+			} else {
+				d.Set("insane_mode_az", "")
+			}
+		} else {
+			d.Set("insane_mode", false)
+			d.Set("insane_mode_az", "")
+		}
+
+		if (gw.CloudType == 1 || gw.CloudType == 256) && gw.EnableVpcDnsServer == "Enabled" {
+			d.Set("enable_vpc_dns_server", true)
+		} else {
+			d.Set("enable_vpc_dns_server", false)
+		}
+
+		peeringHaGateway := &goaviatrix.Gateway{
+			AccountName: d.Get("account_name").(string),
+			GwName:      d.Get("gw_name").(string) + "-hagw",
+		}
+		gwHaGw, _ := client.GetGateway(peeringHaGateway)
+		if gwHaGw != nil {
+			d.Set("peering_ha_cloud_instance_id", gwHaGw.CloudnGatewayInstID)
+			d.Set("peering_ha_gw_name", gwHaGw.GwName)
+			d.Set("peering_ha_public_ip", gwHaGw.PublicIP)
+			d.Set("peering_ha_gw_size", gwHaGw.GwSize)
+			if gwHaGw.CloudType == 1 || gwHaGw.CloudType == 256 {
+				d.Set("peering_ha_subnet", gwHaGw.VpcNet)
+				if gwHaGw.InsaneMode == "yes" {
+					d.Set("peering_ha_insane_mode_az", gwHaGw.GatewayZone)
+				}
+			} else if gwHaGw.CloudType == 8 || gwHaGw.CloudType == 16 {
+				d.Set("peering_ha_subnet", gwHaGw.VpcNet)
+			} else if gwHaGw.CloudType == 4 {
+				d.Set("peering_ha_zone", gwHaGw.GatewayZone)
+			}
+		}
+
+		if gw.CloudType == 1 || gw.CloudType == 256 {
+			tags := &goaviatrix.Tags{
+				ResourceType: "gw",
+				ResourceName: d.Get("gw_name").(string),
+			}
+			if gw.CloudType == 1 {
+				tags.CloudType = 1
+			} else {
+				tags.CloudType = 256
+			}
+
+			tagList, _ := client.GetTags(tags)
+			if tagList != nil && len(tagList) != 0 {
+				d.Set("tag_list", tagList)
+			}
+		}
+
+		if gw.VpnStatus == "enabled" && gw.SplitTunnel == "yes" {
+			splitTunnel := &goaviatrix.SplitTunnel{
+				VpcID: gw.VpcID,
+			}
+			if gw.ElbState == "enabled" {
+				splitTunnel.ElbName = gw.ElbName
+			} else {
+				splitTunnel.ElbName = gw.GwName
+			}
+			splitTunnel1, _ := client.GetSplitTunnel(splitTunnel)
+			if splitTunnel1 != nil {
+				d.Set("name_servers", splitTunnel1.NameServers)
+				d.Set("search_domains", splitTunnel1.SearchDomains)
+				d.Set("additional_cidrs", splitTunnel1.AdditionalCidrs)
+			}
+		} else {
+			d.Set("name_servers", "")
+			d.Set("search_domains", "")
+			d.Set("additional_cidrs", "")
+		}
 	}
 
 	d.SetId(gateway.GwName)
