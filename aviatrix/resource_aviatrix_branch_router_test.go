73c504357be02ba7a2c6363733fd160bb0cee7c9
Cyrus Javan <javan.cyrus@gmail.com>
2020-06-10 01:05:22 -0700

    Add branch router tag resource (#435)

--- aviatrix/resource_aviatrix_branch_router_test.go
+++ aviatrix/resource_aviatrix_branch_router_test.go
@@ -2,12 +2,13 @@ package aviatrix
 
 import (
 	"fmt"
+	"os"
+	"testing"
+
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"github.com/terraform-providers/terraform-provider-aviatrix/goaviatrix"
-	"os"
-	"testing"
 )
 
 func TestAccAviatrixBranchRouter_basic(t *testing.T) {
