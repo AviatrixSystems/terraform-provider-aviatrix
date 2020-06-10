2b144b967a15d7b05d1256a5589144d9c5c2219d
Zhiqiang Jin <zjin82@gmail.com>
2020-06-10 15:18:40 -0700

    Fix acceptance test for vpn_user and transit_external_device_conn

--- aviatrix/resource_aviatrix_transit_external_device_conn.go
+++ aviatrix/resource_aviatrix_transit_external_device_conn.go
@@ -450,6 +450,7 @@ func resourceAviatrixTransitExternalDeviceConnRead(d *schema.ResourceData, meta 
 			}
 		} else {
 			d.Set("ha_enabled", false)
+			d.Set("backup_direct_connect", false)
 		}
 
 		if conn.EnableEdgeSegmentation == "enabled" {
