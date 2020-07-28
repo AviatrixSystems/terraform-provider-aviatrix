d34011e0047cbf6d5b40d844aad0d5cf53764d8e
Cyrus Javan <javan.cyrus@gmail.com>
2020-07-27 17:53:32 -0700

    (16147) Add fqdn_pass_through resource (#492)
    * Add fqdn_pass_through resource
    
    * Fix ordering issues

---
+++ aviatrix/resource_aviatrix_fqdn_pass_through.go
@@ -0,0 +1,123 @@
+package aviatrix
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"github.com/terraform-providers/terraform-provider-aviatrix/goaviatrix"
+)
+
+func resourceAviatrixFQDNPassThrough() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAviatrixFQDNPassThroughCreate,
+		Read:   resourceAviatrixFQDNPassThroughRead,
+		Update: resourceAviatrixFQDNPassThroughUpdate,
+		Delete: resourceAviatrixFQDNPassThroughDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"gw_name": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "Gateway to apply FQDN pass-through rules to.",
+			},
+			"pass_through_cidrs": {
+				Type:        schema.TypeSet,
+				Required:    true,
+				Description: "CIDRs to allow originating requests to ignore FQDN filtering rules.",
+				MinItems:    1,
+				Elem: &schema.Schema{
+					Type:         schema.TypeString,
+					ValidateFunc: validation.IsCIDR,
+				},
+			},
+		},
+	}
+}
+
+func resourceAviatrixFQDNPassThroughCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*goaviatrix.Client)
+
+	gw := &goaviatrix.Gateway{GwName: d.Get("gw_name").(string)}
+	var cidrs []string
+	for _, v := range d.Get("pass_through_cidrs").(*schema.Set).List() {
+		cidrs = append(cidrs, v.(string))
+	}
+	if err := client.ConfigureFQDNPassThroughCIDRs(gw, cidrs); err != nil {
+		return err
+	}
+
+	d.SetId(gw.GwName)
+	return nil
+}
+
+func resourceAviatrixFQDNPassThroughRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*goaviatrix.Client)
+
+	gwName := d.Get("gw_name").(string)
+	if gwName == "" {
+		id := d.Id()
+		log.Printf("[DEBUG] Looks like an import, no fqdn_pass_through gwName received. Import Id is %s", id)
+		d.SetId(id)
+		gwName = id
+	}
+	gw := &goaviatrix.Gateway{GwName: gwName}
+	cidrs, err := client.GetFQDNPassThroughCIDRs(gw)
+	if err == goaviatrix.ErrNotFound {
+		d.SetId("")
+		return nil
+	}
+	if err != nil {
+		return fmt.Errorf("could not find fqdn_pass_through %s: %v", gwName, err)
+	}
+
+	d.Set("gw_name", gwName)
+
+	// CIDRs returned from the API are in the form: <cidr>~~<tags>
+	// The tags are irrelevant so we will remove them before saving the CIDRs to the state file.
+	var cidrsWithoutTags []string
+	for _, cidr := range cidrs {
+		cidrsWithoutTags = append(cidrsWithoutTags, strings.Split(cidr, "~~")[0])
+	}
+
+	err = d.Set("pass_through_cidrs", cidrsWithoutTags)
+	if err != nil {
+		return fmt.Errorf("could not set pass_through_cidrs: %v", err)
+	}
+
+	return nil
+}
+
+func resourceAviatrixFQDNPassThroughUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*goaviatrix.Client)
+
+	if d.HasChange("pass_through_cidrs") {
+		gw := &goaviatrix.Gateway{GwName: d.Get("gw_name").(string)}
+		var cidrs []string
+		for _, v := range d.Get("pass_through_cidrs").(*schema.Set).List() {
+			cidrs = append(cidrs, v.(string))
+		}
+		if err := client.ConfigureFQDNPassThroughCIDRs(gw, cidrs); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func resourceAviatrixFQDNPassThroughDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*goaviatrix.Client)
+
+	gw := &goaviatrix.Gateway{GwName: d.Get("gw_name").(string)}
+	if err := client.DisableFQDNPassThrough(gw); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ website/docs/r/aviatrix_fqdn_pass_through.html.markdown
@@ -0,0 +1,43 @@
+---
+layout: "aviatrix"
+page_title: "Aviatrix: aviatrix_fqdn_pass_through"
+description: |-
+  Manages Aviatrix FQDN filter pass-through
+---
+
+# aviatrix_fqdn_pass_through
+
+The **aviatrix_fqdn_pass_through** resource manages FQDN filter pass-through for Aviatrix gateways.
+
+~> **NOTE:** The **aviatrix_fqdn_pass_through** resource must be created after the **aviatrix_fqdn** resource to be created successfully. To ensure that Terraform orders the creation of these resources correctly please use a `depends_on` meta-argument so that the **aviatrix_fqdn** resource is created before the **aviatrix_fqdn_pass_through** resource.
+
+## Example Usage
+
+```hcl
+# Create an Aviatrix Gateway FQDN filter pass-through
+resource "aviatrix_fqdn_pass_through" "test_fqdn_pass_through" {
+  gw_name            = aviatrix_gateway.test_gw_aws.gw_name
+  pass_through_cidrs = [
+    "10.0.0.0/24",
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+  ]
+
+  depends_on         = [aviatrix_fqdn.test_fqdn]
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `gw_name` - (Required) Gateway name to apply pass-through rules to.
+* `pass_through_cidrs` - (Required) List of origin CIDR's to allow to pass-through FQDN filtering rules. Minimum list length: 1.
+
+## Import
+
+**fqdn_pass_through** can be imported using the `gw_name`, e.g.
+
+```
+$ terraform import aviatrix_fqdn_pass_through.test gw_name
+```
