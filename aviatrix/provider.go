4681a3663ce09e05fed1e13bf22ce4d9745e6b2c
Cyrus Javan <javan.cyrus@gmail.com>
2020-08-11 16:12:07 -0700

    Update supported version to 6.2 (#514)

--- aviatrix/provider.go
+++ aviatrix/provider.go
@@ -8,7 +8,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
-var supportedVersions = []string{"6.1"}
+var supportedVersions = []string{"6.2"}
 
 // Provider returns a schema.Provider for Aviatrix.
 func Provider() terraform.ResourceProvider {
