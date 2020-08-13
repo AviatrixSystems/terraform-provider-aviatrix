cbbc25f6eeaa72567fd958e06c7e390c10e398be
Cyrus Javan <javan.cyrus@gmail.com>
2020-08-13 12:40:21 -0700

    rename branch_router_tag to device_tag

--- goaviatrix/device_tag.go
+++ goaviatrix/device_tag.go
@@ -7,20 +7,20 @@ import (
 	"strings"
 )
 
-type BranchRouterTag struct {
-	Name           string `form:"tag_name,omitempty"`
-	Config         string `form:"custom_cfg,omitempty"`
-	Branches       []string
-	BranchesString string `form:"include_branch_list,omitempty"`
-	CID            string `form:"CID"`
-	Action         string `form:"action"`
+type DeviceTag struct {
+	Name          string `form:"tag_name,omitempty"`
+	Config        string `form:"custom_cfg,omitempty"`
+	Devices       []string
+	DevicesString string `form:"include_device_list,omitempty"`
+	CID           string `form:"CID"`
+	Action        string `form:"action"`
 }
 
-func (c *Client) CreateBranchRouterTag(brt *BranchRouterTag) error {
+func (c *Client) CreateDeviceTag(deviceTag *DeviceTag) error {
 	// Create the tag
-	brt.CID = c.CID
-	brt.Action = "add_cloudwan_configtag"
-	resp, err := c.Post(c.baseURL, brt)
+	deviceTag.CID = c.CID
+	deviceTag.Action = "add_cloudwan_configtag"
+	resp, err := c.Post(c.baseURL, deviceTag)
 
 	if err != nil {
 		return errors.New("HTTP Post add_cloudwan_configtag failed: " + err.Error())
@@ -46,24 +46,24 @@ func (c *Client) CreateBranchRouterTag(brt *BranchRouterTag) error {
 	}
 
 	// Set the tag config
-	if err := c.UpdateBranchRouterTagConfig(brt); err != nil {
+	if err := c.UpdateDeviceTagConfig(deviceTag); err != nil {
 		return err
 	}
 
-	// Attach the branches to the tag
-	if err := c.UpdateBranchRouterTagBranches(brt); err != nil {
+	// Attach the devices to the tag
+	if err := c.AttachDeviceTag(deviceTag); err != nil {
 		return err
 	}
 
-	// Commit the tag config to the branches
-	if err := c.CommitBranchRouterTag(brt); err != nil {
+	// Commit the tag config to the devices
+	if err := c.CommitDeviceTag(deviceTag); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func (c *Client) GetBranchRouterTag(brt *BranchRouterTag) (*BranchRouterTag, error) {
+func (c *Client) GetDeviceTag(brt *DeviceTag) (*DeviceTag, error) {
 	// Check if a tag exists with the given name
 	brt.CID = c.CID
 	brt.Action = "list_cloudwan_configtag_names"
@@ -128,12 +128,12 @@ func (c *Client) GetBranchRouterTag(brt *BranchRouterTag) (*BranchRouterTag, err
 		return nil, errors.New("Rest API get_cloudwan_configtag_details Post failed: " + detailsData.Reason)
 	}
 
-	brt.Branches = detailsData.Results.AttachedBranches
+	brt.Devices = detailsData.Results.AttachedBranches
 	brt.Config = detailsData.Results.Config
 	return brt, nil
 }
 
-func (c *Client) UpdateBranchRouterTagConfig(brt *BranchRouterTag) error {
+func (c *Client) UpdateDeviceTagConfig(brt *DeviceTag) error {
 	brt.CID = c.CID
 	brt.Action = "edit_cloudwan_configtag"
 	resp, err := c.Post(c.baseURL, brt)
@@ -164,14 +164,14 @@ func (c *Client) UpdateBranchRouterTagConfig(brt *BranchRouterTag) error {
 	return nil
 }
 
-func (c *Client) UpdateBranchRouterTagBranches(brt *BranchRouterTag) error {
+func (c *Client) AttachDeviceTag(brt *DeviceTag) error {
 	brt.CID = c.CID
-	brt.Action = "attach_branches_to_cloudwan_configtag"
-	brt.BranchesString = strings.Join(brt.Branches, ", ")
+	brt.Action = "attach_devices_to_cloudwan_configtag"
+	brt.DevicesString = strings.Join(brt.Devices, ", ")
 	resp, err := c.Post(c.baseURL, brt)
 
 	if err != nil {
-		return errors.New("HTTP Post attach_branches_to_cloudwan_configtag failed: " + err.Error())
+		return errors.New("HTTP Post attach_devices_to_cloudwan_configtag failed: " + err.Error())
 	}
 
 	type Resp struct {
@@ -184,26 +184,26 @@ func (c *Client) UpdateBranchRouterTagBranches(brt *BranchRouterTag) error {
 	var b bytes.Buffer
 	_, err = b.ReadFrom(resp.Body)
 	if err != nil {
-		return errors.New("Reading response body attach_branches_to_cloudwan_configtag failed: " + err.Error())
+		return errors.New("Reading response body attach_devices_to_cloudwan_configtag failed: " + err.Error())
 	}
 
 	if err = json.NewDecoder(&b).Decode(&data); err != nil {
-		return errors.New("Json Decode attach_branches_to_cloudwan_configtag failed: " + err.Error() + "\n Body: " + b.String())
+		return errors.New("Json Decode attach_devices_to_cloudwan_configtag failed: " + err.Error() + "\n Body: " + b.String())
 	}
 	if !data.Return {
-		return errors.New("Rest API attach_branches_to_cloudwan_configtag Post failed: " + data.Reason)
+		return errors.New("Rest API attach_devices_to_cloudwan_configtag Post failed: " + data.Reason)
 	}
 
 	return nil
 }
 
-func (c *Client) CommitBranchRouterTag(brt *BranchRouterTag) error {
+func (c *Client) CommitDeviceTag(brt *DeviceTag) error {
 	brt.CID = c.CID
-	brt.Action = "commit_cloudwan_configtag_to_branches"
+	brt.Action = "commit_cloudwan_configtag_to_devices"
 	resp, err := c.Post(c.baseURL, brt)
 
 	if err != nil {
-		return errors.New("HTTP Post commit_cloudwan_configtag_to_branches failed: " + err.Error())
+		return errors.New("HTTP Post commit_cloudwan_configtag_to_devices failed: " + err.Error())
 	}
 
 	type Resp struct {
@@ -216,20 +216,20 @@ func (c *Client) CommitBranchRouterTag(brt *BranchRouterTag) error {
 	var b bytes.Buffer
 	_, err = b.ReadFrom(resp.Body)
 	if err != nil {
-		return errors.New("Reading response body commit_cloudwan_configtag_to_branches failed: " + err.Error())
+		return errors.New("Reading response body commit_cloudwan_configtag_to_devices failed: " + err.Error())
 	}
 
 	if err = json.NewDecoder(&b).Decode(&data); err != nil {
-		return errors.New("Json Decode commit_cloudwan_configtag_to_branches failed: " + err.Error() + "\n Body: " + b.String())
+		return errors.New("Json Decode commit_cloudwan_configtag_to_devices failed: " + err.Error() + "\n Body: " + b.String())
 	}
 	if !data.Return {
-		return errors.New("Rest API commit_cloudwan_configtag_to_branches Post failed: " + data.Reason)
+		return errors.New("Rest API commit_cloudwan_configtag_to_devices Post failed: " + data.Reason)
 	}
 
 	return nil
 }
 
-func (c *Client) DeleteBranchRouterTag(brt *BranchRouterTag) error {
+func (c *Client) DeleteDeviceTag(brt *DeviceTag) error {
 	brt.CID = c.CID
 	brt.Action = "delete_cloudwan_configtag"
 	resp, err := c.Post(c.baseURL, brt)
