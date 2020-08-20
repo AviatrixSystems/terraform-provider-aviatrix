a6cca1b7ebaeca55f660b0cc08cb4b59e35aaa78
Zhiqiang Jin <zjin82@gmail.com>
2020-08-19 18:59:31 -0700

    Revert "0016823: Add support of editing gw_name for aviatrix_transit_gateway (#526)"
    This reverts commit a17dd1f72f5ab557a6b17a0cd0d186971d284fa5.

--- aviatrix/resource_aviatrix_transit_gateway.go
+++ aviatrix/resource_aviatrix_transit_gateway.go
@@ -286,11 +286,6 @@ func resourceAviatrixTransitGateway() *schema.Resource {
 				Default:     false,
 				Description: "Enable segmentation to allow association of transit gateway to security domains.",
 			},
-			"gw_original_name": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "Name of the transit gateway when it was created.",
-			},
 		},
 	}
 }
@@ -740,7 +735,7 @@ func resourceAviatrixTransitGatewayRead(d *schema.ResourceData, meta interface{}
 	client := meta.(*goaviatrix.Client)
 
 	var isImport bool
-	gwName := d.Get("gw_original_name").(string)
+	gwName := d.Get("gw_name").(string)
 	if gwName == "" {
 		isImport = true
 		id := d.Id()
@@ -752,7 +747,7 @@ func resourceAviatrixTransitGatewayRead(d *schema.ResourceData, meta interface{}
 
 	gateway := &goaviatrix.Gateway{
 		AccountName: d.Get("account_name").(string),
-		GwName:      gwName,
+		GwName:      d.Get("gw_name").(string),
 	}
 
 	gw, err := client.GetGateway(gateway)
@@ -770,7 +765,6 @@ func resourceAviatrixTransitGatewayRead(d *schema.ResourceData, meta interface{}
 		d.Set("cloud_type", gw.CloudType)
 		d.Set("account_name", gw.AccountName)
 		d.Set("gw_name", gw.GwName)
-		d.Set("gw_original_name", gw.GwOriginalName)
 		d.Set("subnet", gw.VpcNet)
 
 		if gw.CloudType == goaviatrix.AWS {
@@ -1062,6 +1056,9 @@ func resourceAviatrixTransitGatewayUpdate(d *schema.ResourceData, meta interface
 	}
 	if d.HasChange("account_name") {
 		return fmt.Errorf("updating account_name is not allowed")
+	}
+	if d.HasChange("gw_name") {
+		return fmt.Errorf("updating gw_name is not allowed")
 	}
 	if d.HasChange("vpc_id") {
 		return fmt.Errorf("updating vpc_id is not allowed")
@@ -1731,19 +1728,6 @@ func resourceAviatrixTransitGatewayUpdate(d *schema.ResourceData, meta interface
 				return fmt.Errorf("could not disable segmentation: %v", err)
 			}
 		}
-	}
-
-	if d.HasChange("gw_name") {
-		gwOriginalName, gwName := d.GetChange("gw_name")
-		gateway := &goaviatrix.Gateway{
-			GwOriginalName: gwOriginalName.(string),
-			GwName:         gwName.(string),
-		}
-		err := client.UpdateGatewayAlias(gateway)
-		if err != nil {
-			return err
-		}
-		d.SetId(gateway.GwName)
 	}
 
 	d.Partial(false)
