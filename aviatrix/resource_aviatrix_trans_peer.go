bbc84893dccdf8fb9e66b4839445874701a00d62
ZHIQIANG JIN <zjin@aviatrix.com>
2020-08-28 23:03:31 -0700

    0016823: Initial implementation of support for updating 'source' and 'nexthop' in aviatrix_trans_peer (#548)

--- aviatrix/resource_aviatrix_trans_peer.go
+++ aviatrix/resource_aviatrix_trans_peer.go
@@ -13,6 +13,7 @@ func resourceAviatrixTransPeer() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceAviatrixTransPeerCreate,
 		Read:   resourceAviatrixTransPeerRead,
+		Update: resourceAviatrixTransPeerUpdate,
 		Delete: resourceAviatrixTransPeerDelete,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
@@ -22,13 +23,11 @@ func resourceAviatrixTransPeer() *schema.Resource {
 			"source": {
 				Type:        schema.TypeString,
 				Required:    true,
-				ForceNew:    true,
-				Description: "Name of Source gateway.",
+				Description: "Name of source gateway.",
 			},
 			"nexthop": {
 				Type:        schema.TypeString,
 				Required:    true,
-				ForceNew:    true,
 				Description: "Name of nexthop gateway.",
 			},
 			"reachable_cidr": {
@@ -36,6 +35,16 @@ func resourceAviatrixTransPeer() *schema.Resource {
 				Required:    true,
 				ForceNew:    true,
 				Description: "Destination CIDR.",
+			},
+			"source_original_name": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Name of the source gateway when it was created.",
+			},
+			"nexthop_original_name": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Name of the nexthop gateway when it was created.",
 			},
 		},
 	}
@@ -65,23 +74,28 @@ func resourceAviatrixTransPeerRead(d *schema.ResourceData, meta interface{}) err
 	client := meta.(*goaviatrix.Client)
 
 	sourceGw := d.Get("source").(string)
-	nestHopGw := d.Get("nexthop").(string)
+	nextHopGw := d.Get("nexthop").(string)
 	reachableCIDR := d.Get("reachable_cidr").(string)
 
-	if sourceGw == "" || nestHopGw == "" || reachableCIDR == "" {
+	if sourceGw == "" || nextHopGw == "" || reachableCIDR == "" {
 		id := d.Id()
 		log.Printf("[DEBUG] Looks like an import, no transit gateway names or reachable cidr received. "+
 			"Import Id is %s", id)
 		d.Set("source", strings.Split(id, "~")[0])
 		d.Set("nexthop", strings.Split(id, "~")[1])
 		d.Set("reachable_cidr", strings.Split(id, "~")[2])
+		sourceGw = strings.Split(id, "~")[0]
+		nextHopGw = strings.Split(id, "~")[1]
+		reachableCIDR = strings.Split(id, "~")[2]
 		d.SetId(id)
 	}
 
 	transPeer := &goaviatrix.TransPeer{
-		Source:        d.Get("source").(string),
-		Nexthop:       d.Get("nexthop").(string),
-		ReachableCidr: d.Get("reachable_cidr").(string),
+		Source:              sourceGw,
+		Nexthop:             nextHopGw,
+		ReachableCidr:       reachableCIDR,
+		SourceOriginalName:  d.Get("source_original_name").(string),
+		NexthopOriginalName: d.Get("nexthop_original_name").(string),
 	}
 	transPeer, err := client.GetTransPeer(transPeer)
 	if err != nil {
@@ -95,8 +109,47 @@ func resourceAviatrixTransPeerRead(d *schema.ResourceData, meta interface{}) err
 	d.Set("source", transPeer.Source)
 	d.Set("nexthop", transPeer.Nexthop)
 	d.Set("reachable_cidr", transPeer.ReachableCidr)
+	d.Set("source_original_name", transPeer.SourceOriginalName)
+	d.Set("nexthop_original_name", transPeer.NexthopOriginalName)
 
 	return nil
+}
+
+func resourceAviatrixTransPeerUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*goaviatrix.Client)
+
+	if d.HasChange("source") {
+		_, gwNameNew := d.GetChange("source")
+		gateway := &goaviatrix.Gateway{
+			GwName:         gwNameNew.(string),
+			GwOriginalName: d.Get("source_original_name").(string),
+		}
+		err := client.IsGatewayNameUpdatable(gateway)
+		if err != nil {
+			return nil
+		}
+	}
+
+	if d.HasChange("nexthop") {
+		_, gwNameNew := d.GetChange("nexthop")
+		gateway := &goaviatrix.Gateway{
+			GwName:         gwNameNew.(string),
+			GwOriginalName: d.Get("nexthop_original_name").(string),
+		}
+		err := client.IsGatewayNameUpdatable(gateway)
+		if err != nil {
+			return err
+		}
+	}
+
+	transPeer := &goaviatrix.TransPeer{
+		Source:        d.Get("source").(string),
+		Nexthop:       d.Get("nexthop").(string),
+		ReachableCidr: d.Get("reachable_cidr").(string),
+	}
+
+	d.SetId(transPeer.Source + "~" + transPeer.Nexthop + "~" + transPeer.ReachableCidr)
+	return resourceAviatrixTransPeerRead(d, meta)
 }
 
 func resourceAviatrixTransPeerDelete(d *schema.ResourceData, meta interface{}) error {
