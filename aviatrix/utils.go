3f99047d6128b5dbb50a328416efe83ed59d9c44
Yuhao Yang <kaokaoyang030@gmail.com>
2020-10-08 19:22:12 -0700

    update imports

--- aviatrix/utils.go
+++ aviatrix/utils.go
@@ -2,11 +2,11 @@ package aviatrix
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/terraform-providers/terraform-provider-aviatrix/goaviatrix"
-	"strings"
 )
 
 // validateAzureAZ is a SchemaValidateFunc for Azure Availability Zone
