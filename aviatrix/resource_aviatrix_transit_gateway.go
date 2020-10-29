53f797d8afdcd38ea17195b3dd7defa73f471047
Yuhao Yang <62894834+doufudoufuyang@users.noreply.github.com>
2020-10-29 16:14:31 -0700

    Fixing return error issue (#629)
    * fixing return error issue when the enable_transit_firenet is set to false
    
    * use WARN log instead of return ERROR
    
    Co-authored-by: Yuhao Yang <“kaokaoyang030@gmail.com“>

--- aviatrix/resource_aviatrix_transit_gateway.go
+++ aviatrix/resource_aviatrix_transit_gateway.go
@@ -1009,7 +1009,7 @@ func resourceAviatrixTransitGatewayRead(d *schema.ResourceData, meta interface{}
 
 		lanCidr, err := client.GetTransitGatewayLanCidr(gw.GwName)
 		if err != nil && err != goaviatrix.ErrNotFound {
-			return fmt.Errorf("couldn't find LAN Interface Cidr of Transit Gateway: %s", err)
+			log.Printf("[WARN] Error getting lan cidr for transit gateway %s due to %s", gw.GwName, err)
 		}
 		d.Set("lan_interface_cidr", lanCidr)
 	}
