8628fb121c7b59c9f628eff67d6832f3ea49bf53
Cyrus Javan <javan.cyrus@gmail.com>
2020-10-28 19:04:35 -0700

    18330: firenet manage_firewall_instance_association state upgrade

---
+++ aviatrix/resource_aviatrix_firenet_migrate.go
@@ -0,0 +1,23 @@
+package aviatrix
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceAviatrixFireNetResourceV0() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"manage_firewall_instance_association": {
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+		},
+	}
+}
+
+func resourceAviatrixFireNetStateUpgradeV0(rawState map[string]interface{}, meta interface{}) (map[string]interface{}, error) {
+	if _, ok := rawState["manage_firewall_instance_association"]; !ok {
+		rawState["manage_firewall_instance_association"] = true
+	}
+	return rawState, nil
+}
