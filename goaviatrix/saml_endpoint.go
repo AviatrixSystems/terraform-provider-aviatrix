07b9e9f9f71d812ebbe7d48cf50875024e600dd7
Arvind <21178759+arvindsree@users.noreply.github.com>
2020-10-28 19:46:07 -0700

    16265: [SAML Endpoint] Add support for Sign Authn Requests (#624)
    * 16265: [SAML Endpoint] Add support for Sign Authn Requests
    
    * 16265: Review comment: correct attribute
    
    * 16265: Fix go-lint
    
    * 16265: Fix go-lint

--- goaviatrix/saml_endpoint.go
+++ goaviatrix/saml_endpoint.go
@@ -9,37 +9,39 @@ import (
 )
 
 type SamlEndpoint struct {
-	Action          string   `form:"action,omitempty"`
-	CID             string   `form:"CID,omitempty"`
-	EndPointName    string   `form:"endpoint_name,omitempty" json:"name,omitempty"`
-	IdpMetadataType string   `form:"idp_metadata_type,omitempty" json:"metadata_type,omitempty"`
-	IdpMetadata     string   `form:"idp_metadata,omitempty" json:"idp_metadata,omitempty"`
-	EntityIdType    string   `json:"entity_id,omitempty"`
-	CustomEntityId  string   `form:"entity_id,omitempty" json:"custom_entityID,omitempty"`
-	MsgTemplate     string   `form:"msgtemplate,omitempty" json:"msgtemplate,omitempty"`
-	MsgTemplateType string   `json:"msgtemplate_type,omitempty"`
-	ControllerLogin string   `form:"controller_login,omitempty" json:"controller_login,omitempty"`
-	AccessSetBy     string   `form:"access_ctrl,omitempty" json:"access_ctrl,omitempty"`
-	RbacGroups      string   `form:"groups,omitempty"`
-	RbacGroupsRead  []string `json:"cl_rbac_groups,omitempty"`
+	Action            string   `form:"action,omitempty"`
+	CID               string   `form:"CID,omitempty"`
+	EndPointName      string   `form:"endpoint_name,omitempty" json:"name,omitempty"`
+	IdpMetadataType   string   `form:"idp_metadata_type,omitempty" json:"metadata_type,omitempty"`
+	IdpMetadata       string   `form:"idp_metadata,omitempty" json:"idp_metadata,omitempty"`
+	EntityIdType      string   `json:"entity_id,omitempty"`
+	CustomEntityId    string   `form:"entity_id,omitempty" json:"custom_entityID,omitempty"`
+	MsgTemplate       string   `form:"msgtemplate,omitempty" json:"msgtemplate,omitempty"`
+	MsgTemplateType   string   `json:"msgtemplate_type,omitempty"`
+	ControllerLogin   string   `form:"controller_login,omitempty" json:"controller_login,omitempty"`
+	AccessSetBy       string   `form:"access_ctrl,omitempty" json:"access_ctrl,omitempty"`
+	RbacGroups        string   `form:"groups,omitempty"`
+	RbacGroupsRead    []string `json:"cl_rbac_groups,omitempty"`
+	SignAuthnRequests string   `form:"sign_authn_requests,omitempty"`
 }
 
-type SamlEndpointEdit struct {
-	EndPointName    string   `json:"name,omitempty"`
-	IdpMetadataType string   `json:"metadata_type,omitempty"`
-	IdpMetadata     string   `json:"idp_metadata,omitempty"`
-	EntityIdType    string   `json:"entity_id,omitempty"`
-	CustomEntityId  string   `json:"custom_entityID,omitempty"`
-	MsgTemplate     string   `json:"msgtemplate,omitempty"`
-	MsgTemplateType string   `json:"msgtemplate_type,omitempty"`
-	ControllerLogin bool     `json:"controller_login,omitempty"`
-	AccessSetBy     string   `json:"access_ctrl,omitempty"`
-	RbacGroupsRead  []string `json:"cl_rbac_groups,omitempty"`
+type SamlEndpointInfo struct {
+	EndPointName      string   `json:"name,omitempty"`
+	IdpMetadataType   string   `json:"metadata_type,omitempty"`
+	IdpMetadata       string   `json:"idp_metadata,omitempty"`
+	EntityIdType      string   `json:"entity_id,omitempty"`
+	CustomEntityId    string   `json:"custom_entityID,omitempty"`
+	MsgTemplate       string   `json:"msgtemplate,omitempty"`
+	MsgTemplateType   string   `json:"msgtemplate_type,omitempty"`
+	ControllerLogin   bool     `json:"controller_login,omitempty"`
+	AccessSetBy       string   `json:"access_ctrl,omitempty"`
+	RbacGroupsRead    []string `json:"cl_rbac_groups,omitempty"`
+	SignAuthnRequests bool     `json:"sign_authn_requests,omitempty"`
 }
 
 type SamlResp struct {
 	Return  bool             `json:"return"`
-	Results SamlEndpointEdit `json:"results"`
+	Results SamlEndpointInfo `json:"results"`
 	Reason  string           `json:"reason"`
 }
 
@@ -64,7 +66,7 @@ func (c *Client) CreateSamlEndpoint(samlEndpoint *SamlEndpoint) error {
 	return nil
 }
 
-func (c *Client) GetSamlEndpoint(samlEndpoint *SamlEndpoint) (*SamlEndpoint, error) {
+func (c *Client) GetSamlEndpoint(samlEndpoint *SamlEndpoint) (*SamlEndpointInfo, error) {
 	Url, err := url.Parse(c.baseURL)
 	if err != nil {
 		return nil, errors.New(("url Parsing failed for 'get_saml_endpoint_information': ") + err.Error())
@@ -92,19 +94,8 @@ func (c *Client) GetSamlEndpoint(samlEndpoint *SamlEndpoint) (*SamlEndpoint, err
 		}
 		return nil, errors.New("Rest API 'get_saml_endpoint_information' Get failed: " + data.Reason)
 	}
-	samlEndpoint.CustomEntityId = data.Results.CustomEntityId
-	samlEndpoint.IdpMetadataType = data.Results.IdpMetadataType
-	samlEndpoint.IdpMetadata = data.Results.IdpMetadata
-	samlEndpoint.MsgTemplateType = data.Results.MsgTemplateType
-	samlEndpoint.MsgTemplate = data.Results.MsgTemplate
-	samlEndpoint.AccessSetBy = data.Results.AccessSetBy
-	if data.Results.ControllerLogin {
-		samlEndpoint.ControllerLogin = "yes"
-		if data.Results.AccessSetBy == "controller" && len(data.Results.RbacGroupsRead) != 0 {
-			samlEndpoint.RbacGroups = strings.Join(data.Results.RbacGroupsRead, ",")
-		}
-	}
-	return samlEndpoint, nil
+	data.Results.EndPointName = samlEndpoint.EndPointName
+	return &data.Results, nil
 }
 
 func (c *Client) EditSamlEndpoint(samlEndpoint *SamlEndpoint) error {
