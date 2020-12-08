b003ddfa12afc8610031946280177716b2ae67fc
Shuo Liu <sliu@aviatrix.com>
2020-12-08 11:05:58 -0800

    18667: implemented netflow agent (#671)

---
+++ goaviatrix/netflow_agent.go
@@ -0,0 +1,63 @@
+package goaviatrix
+
+import "strconv"
+
+type NetflowAgent struct {
+	CID                   string
+	ServerIp              string
+	Port                  int
+	Version               int
+	ExcludedGatewaysInput string
+}
+
+type NetflowAgentResp struct {
+	ServerIp         string   `json:"server_ip"`
+	Port             string   `json:"port"`
+	Version          string   `json:"version"`
+	ExcludedGateways []string `json:"excluded_gateway"`
+	Status           string   `json:"status"`
+}
+
+func (c *Client) EnableNetflowAgent(r *NetflowAgent) error {
+	params := map[string]string{
+		"action":               "enable_netflow_agent",
+		"CID":                  c.CID,
+		"server_ip":            r.ServerIp,
+		"port":                 strconv.Itoa(r.Port),
+		"version":              strconv.Itoa(r.Version),
+		"exclude_gateway_list": r.ExcludedGatewaysInput,
+	}
+
+	return c.PostAPI(params["action"], params, BasicCheck)
+}
+
+func (c *Client) GetNetflowAgentStatus() (*NetflowAgentResp, error) {
+	params := map[string]string{
+		"action": "get_netflow_agent",
+		"CID":    c.CID,
+	}
+
+	type Resp struct {
+		Return  bool             `json:"return"`
+		Results NetflowAgentResp `json:"results"`
+		Reason  string           `json:"reason"`
+	}
+
+	var data Resp
+
+	err := c.GetAPI(&data, params["action"], params, BasicCheck)
+	if err != nil {
+		return nil, err
+	}
+
+	return &data.Results, nil
+}
+
+func (c *Client) DisableNetflowAgent() error {
+	params := map[string]string{
+		"action": "disable_netflow_agent",
+		"CID":    c.CID,
+	}
+
+	return c.PostAPI(params["action"], params, BasicCheck)
+}
---
+++ website/docs/r/aviatrix_netflow_agent.html.markdown
@@ -0,0 +1,49 @@
+---
+subcategory: "Settings"
+layout: "aviatrix"
+page_title: "Aviatrix: aviatrix_netflow_agent"
+description: |-
+  Enables and disables netflow_agent
+---
+
+# aviatrix_netflow_agent
+
+The **aviatrix_netflow_agent** resource allows the enabling and disabling of netflow agent.
+
+## Example Usage
+
+```hcl
+# Enable netflow agent
+resource "aviatrix_netflow_agent" "test_netflow_agent" {
+  server_ip         = "1.2.3.4"
+  port              = 10
+  version           = 5
+  excluded_gateways = ["a", "b"]
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+### Required
+* `server_ip` (Required) Netflow server IP address.
+* `port` (Required) Netflow server port.
+
+### Optional
+* `version` (Optional) Netflow version (5 or 9). 5 by default. 
+* `excluded_gateways` (Optional) List of gateways to be excluded from logging. e.g.: ["gateway01", "gateway02", "gateway01-hagw"].
+
+## Attribute Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `status` - The status of netflow agent.
+
+## Import
+
+**netflow_agent** can be imported using "netflow_agent", e.g.
+
+```
+$ terraform import aviatrix_netflow_agent.test netflow_agent
+```
