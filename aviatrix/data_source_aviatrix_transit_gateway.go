f526b3ee44ea2afdd7aa1efe80c4cad37699e849
Shuo Liu <sliu@aviatrix.com>
2021-01-22 14:35:53 -0800

    fix19549: data source will not read ood related attributes if oob disabled

--- aviatrix/data_source_aviatrix_transit_gateway.go
+++ aviatrix/data_source_aviatrix_transit_gateway.go
@@ -294,8 +294,10 @@ func dataSourceAviatrixTransitGatewayRead(d *schema.ResourceData, meta interface
 		d.Set("private_ip", gw.PrivateIP)
 
 		d.Set("enable_private_oob", gw.EnablePrivateOob)
-		d.Set("oob_management_subnet", gw.OobManagementSubnet)
-		d.Set("oob_availability_zone", gw.GatewayZone)
+		if gw.EnablePrivateOob {
+			d.Set("oob_management_subnet", gw.OobManagementSubnet)
+			d.Set("oob_availability_zone", gw.GatewayZone)
+		}
 
 		if gw.EnableNat == "yes" && gw.SnatMode == "primary" {
 			d.Set("single_ip_snat", true)
