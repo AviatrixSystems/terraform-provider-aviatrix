55fb0f4e0f3c160fced370b22c9c2bf019972b6d
Matthew Lin <78760405+mlin-aviatrix@users.noreply.github.com>
2021-03-03 13:46:10 -0800

    20384: Add aviatrix_controller_bgp_max_as_limit_config resource (#782)
    * 20384: Create aviatrix_controller_max_as_limit_config resource
    
    * Added aviatrix_controller_bgp_max_as_limit_config to the provider
    
    * 20384: Changed max_as_limit to TypeInt. User has to delete the resource to disable the config.
    
    * 20384: Added documentation for controller BGP mas AS limit config
    
    * 20384: Made resource_aviatrix_controller_bgp_max_as_limit_config context aware
    
    * 20384: SetId on read in aviatrix_controller_bgp_max_as_limit_config
    
    * 20384: Fix typos in aviatrix_controller_bgp_max_as_limit_config
    
    * 20384: Fixed golint error
    
    * 20384: Remove extra checks and fix stylistc errors
    
    * 20384: Added basic acceptance test case for controller BGP Max AS Limit Config
    
    * 20384: Replaced goaviatrix github import
    
    * 20384: Fixed lint error
    
    * 20384: Removed unnecessary fmt.Sprint
    
    * 20384: Return ErrNotFound when feature is disabled, fix acceptance tests and add SKIP_CONTROLLER_BGP_MAX_AS_LIMIT_CONFIG to test-infra.
    
    * 20384: Fix destroy check in acceptance test
    
    * 20384: Fix logic error for destroy check in acceptance test
    
    * 20384: Fix error messages in acceptance tests

---
+++ aviatrix/resource_aviatrix_controller_bgp_max_as_limit_config.go
@@ -0,0 +1,91 @@
+package aviatrix
+
+import (
+	"context"
+	"strings"
+
+	"github.com/AviatrixSystems/terraform-provider-aviatrix/v2/goaviatrix"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+func resourceAviatrixControllerBgpMaxAsLimitConfig() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceAviatrixControllerBgpMaxAsLimitConfigCreate,
+		ReadContext:   resourceAviatrixControllerBgpMaxAsLimitConfigRead,
+		UpdateContext: resourceAviatrixControllerBgpMaxAsLimitConfigUpdate,
+		DeleteContext: resourceAviatrixControllerBgpMaxAsLimitConfigDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"max_as_limit": {
+				Type:         schema.TypeInt,
+				Required:     true,
+				ValidateFunc: validation.IntBetween(1, 254),
+				Description:  "The maximum AS path limit allowed by transit gateways when handling BGP/Peering route propagation.",
+			},
+		},
+	}
+}
+
+func resourceAviatrixControllerBgpMaxAsLimitConfigCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*goaviatrix.Client)
+
+	maxAsLimit := d.Get("max_as_limit").(int)
+	err := client.SetControllerBgpMaxAsLimit(ctx, maxAsLimit)
+	if err != nil {
+		return diag.Errorf("failed to create controller BGP max AS limit config: %v", err)
+	}
+
+	d.SetId(strings.Replace(client.ControllerIP, ".", "-", -1))
+	return resourceAviatrixControllerBgpMaxAsLimitConfigRead(ctx, d, meta)
+}
+
+func resourceAviatrixControllerBgpMaxAsLimitConfigRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*goaviatrix.Client)
+
+	if d.Id() != strings.Replace(client.ControllerIP, ".", "-", -1) {
+		return diag.Errorf("ID: %s does not match controller IP. Please provide correct ID for importing", d.Id())
+	}
+
+	maxAsLimit, err := client.GetControllerBgpMaxAsLimit(ctx)
+	if err != nil {
+		if err == goaviatrix.ErrNotFound {
+			d.SetId("")
+			return nil
+		}
+		return diag.Errorf("failed to get controller BGP max AS limit config: %v", err)
+	}
+
+	d.Set("max_as_limit", maxAsLimit)
+	d.SetId(strings.Replace(client.ControllerIP, ".", "-", -1))
+	return nil
+}
+
+func resourceAviatrixControllerBgpMaxAsLimitConfigUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*goaviatrix.Client)
+
+	if d.HasChange("max_as_limit") {
+		maxAsLimit := d.Get("max_as_limit").(int)
+		err := client.SetControllerBgpMaxAsLimit(ctx, maxAsLimit)
+		if err != nil {
+			return diag.Errorf("failed to update controller BGP max AS limit config: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func resourceAviatrixControllerBgpMaxAsLimitConfigDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*goaviatrix.Client)
+
+	err := client.DisableControllerBgpMaxAsLimit(ctx)
+	if err != nil {
+		return diag.Errorf("failed to delete controller BGP max AS limit config: %v", err)
+	}
+
+	return nil
+}
---
+++ docs/resources/aviatrix_controller_bgp_max_as_limit_config.md
@@ -0,0 +1,35 @@
+---
+subcategory: "Settings"
+layout: "aviatrix"
+page_title: "Aviatrix: aviatrix_controller_bgp_max_as_limit_config"
+description: |-
+  Creates and manages an Aviatrix controller BGP max AS limit for transit gateways
+---
+
+# aviatrix_controller_bgp_max_as_limit_config
+
+The **aviatrix_controller_bgp_max_as_limit_config** resource allows management of an Aviatrix Controller's BGP max AS limit for transit gateways. This resource is available as of provider version R2.18.1+.
+
+## Example Usage
+
+```hcl
+# Create an Aviatrix Controller BGP max AS limit config
+resource "aviatrix_controller_bgp_max_as_limit_config" "test_max_as_limit" {
+  max_as_limit = 1
+}
+```
+
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `max_as_limit` - The maximum AS path limit allowed by transit gateways when handling BGP/Peering route propagation. Must be a number in the range [1-254].
+
+## Import
+
+**aviatrix_controller_bgp_max_as_limit_config** can be imported using controller IP, e.g. controller IP is : 10.11.12.13
+
+```
+$ terraform import aviatrix_controller_bgp_max_as_limit_config.test_max_as_limit 10-11-12-13
+```
