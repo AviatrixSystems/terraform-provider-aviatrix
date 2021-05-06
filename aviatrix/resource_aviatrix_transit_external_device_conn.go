38b7dfd57cd548a00f1248574fc5d9f7c8ba446c
Shuo Liu <sliu@aviatrix.com>
2021-05-06 09:27:52 -0700

    simplified

--- aviatrix/resource_aviatrix_transit_external_device_conn.go
+++ aviatrix/resource_aviatrix_transit_external_device_conn.go
@@ -304,7 +304,6 @@ func resourceAviatrixTransitExternalDeviceConn() *schema.Resource {
 					ip := d.Get("remote_gateway_ip").(string)
 					o, n := d.GetChange("phase1_remote_identifier")
 					return n.(string) == ip && o.(string) == ip
-
 				},
 				Description:  "Phase 1 remote identifier of the IPsec tunnel.",
 				ValidateFunc: validation.IsIPv4Address,
@@ -761,9 +760,7 @@ func resourceAviatrixTransitExternalDeviceConnRead(d *schema.ResourceData, meta 
 	if err != nil {
 		return fmt.Errorf("couldn't find Aviatrix Site2Cloud: %s, %#v", err, s2c)
 	}
-	if s2c != nil {
-		d.Set("phase1_remote_identifier", s2c.Phase1RemoteIdentifier)
-	}
+	d.Set("phase1_remote_identifier", s2c.Phase1RemoteIdentifier)
 
 	d.SetId(conn.ConnectionName + "~" + conn.VpcID)
 	return nil
