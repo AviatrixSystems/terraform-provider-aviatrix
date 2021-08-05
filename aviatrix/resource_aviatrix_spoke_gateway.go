bb70927e906393b5c289f563523c30b49338e10d
Cyrus Javan <javan.cyrus@gmail.com>
2021-08-04 17:24:28 -0700

    AVX-14056: Serial upgrade for HA/primary downgrade
    Only do parallel ha/primary upgrade if user is only
    doing a software upgrade.

--- aviatrix/resource_aviatrix_spoke_gateway.go
+++ aviatrix/resource_aviatrix_spoke_gateway.go
@@ -2130,20 +2130,34 @@ func resourceAviatrixSpokeGatewayUpdate(d *schema.ResourceData, meta interface{}
 	primaryHasImageVersionChange := d.HasChange("image_version")
 	haHasImageVersionChange := d.HasChange("ha_image_version")
 	if primaryHasVersionChange || haHasVersionChange {
-		if primaryHasVersionChange && haHasVersionChange && !primaryHasImageVersionChange && !haHasImageVersionChange {
-			// Both Primary and HA have changed just their software_version
+		// To determine if this is an attempted software rollback, we check if
+		// old is a higher version than new. Or, the new version is the
+		// special string "previous".
+		oldPrimarySoftwareVersion, newPrimarySoftwareVersion := d.GetChange("software_version")
+		comparePrimary, err := goaviatrix.CompareSoftwareVersions(oldPrimarySoftwareVersion.(string), newPrimarySoftwareVersion.(string))
+		primaryRollbackSoftwareVersion := (err == nil && comparePrimary > 0) || newPrimarySoftwareVersion == "previous"
+
+		oldHaSoftwareVersion, newHaSoftwareVersion := d.GetChange("ha_software_version")
+		compareHa, err := goaviatrix.CompareSoftwareVersions(oldHaSoftwareVersion.(string), newHaSoftwareVersion.(string))
+		haRollbackSoftwareVersion := (err == nil && compareHa > 0) || newHaSoftwareVersion == "previous"
+
+		if primaryHasVersionChange && haHasVersionChange &&
+			!primaryHasImageVersionChange && !haHasImageVersionChange &&
+			!primaryRollbackSoftwareVersion && !haRollbackSoftwareVersion {
+			// Both Primary and HA have upgraded just their software_version
 			// so we can perform upgrade in parallel.
+			log.Printf("[INFO] Upgrading spoke gateway gw_name=%s ha/primary pair in parallel", gateway.GwName)
 			swVersion := d.Get("software_version").(string)
 			imageVersion := d.Get("image_version").(string)
 			gw := &goaviatrix.Gateway{
-				GwName:          d.Get("gw_name").(string),
+				GwName:          gateway.GwName,
 				SoftwareVersion: swVersion,
 				ImageVersion:    imageVersion,
 			}
 			haSwVersion := d.Get("ha_software_version").(string)
 			haImageVersion := d.Get("ha_image_version").(string)
 			hagw := &goaviatrix.Gateway{
-				GwName:          d.Get("gw_name").(string) + "-hagw",
+				GwName:          gateway.GwName + "-hagw",
 				SoftwareVersion: haSwVersion,
 				ImageVersion:    haImageVersion,
 			}
@@ -2169,12 +2183,13 @@ func resourceAviatrixSpokeGatewayUpdate(d *schema.ResourceData, meta interface{}
 			} else if haErr != nil {
 				return fmt.Errorf("could not upgrade HA spoke gateway ha_software_version=%s: %v", haSwVersion, haErr)
 			}
-		} else { // Only primary or only HA has changed, or they have changed image_version
+		} else { // Only primary or only HA has changed, or image_version changed, or it is a software rollback
+			log.Printf("[INFO] Upgrading spoke gateway gw_name=%s ha or primary in serial", gateway.GwName)
 			if primaryHasVersionChange {
 				swVersion := d.Get("software_version").(string)
 				imageVersion := d.Get("image_version").(string)
 				gw := &goaviatrix.Gateway{
-					GwName:          d.Get("gw_name").(string),
+					GwName:          gateway.GwName,
 					SoftwareVersion: swVersion,
 					ImageVersion:    imageVersion,
 				}
@@ -2187,7 +2202,7 @@ func resourceAviatrixSpokeGatewayUpdate(d *schema.ResourceData, meta interface{}
 				haSwVersion := d.Get("ha_software_version").(string)
 				haImageVersion := d.Get("ha_image_version").(string)
 				hagw := &goaviatrix.Gateway{
-					GwName:          d.Get("gw_name").(string) + "-hagw",
+					GwName:          gateway.GwName + "-hagw",
 					SoftwareVersion: haSwVersion,
 					ImageVersion:    haImageVersion,
 				}
--- goaviatrix/version_test.go
+++ goaviatrix/version_test.go
@@ -18,9 +18,11 @@ func TestParseVersion(t *testing.T) {
 			"UserConnect-6.3-patch.2309",
 			"6.3",
 			&AviatrixVersion{
-				Major: 6,
-				Minor: 3,
-				Build: 2309,
+				Major:        6,
+				Minor:        3,
+				Build:        2309,
+				MinorBuildID: "patch",
+				HasBuild:     true,
 			},
 			false,
 		},
@@ -37,6 +39,115 @@ func TestParseVersion(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got1, tt.want1) {
 				t.Errorf("ParseVersion() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func TestCompareSoftwareVersions(t *testing.T) {
+	tt := []struct {
+		name    string
+		a       string
+		b       string
+		want    int
+		wantErr bool
+	}{
+		{
+			name: "test01",
+			a:    "6.5.100",
+			b:    "6.5.101",
+			want: -1,
+		},
+		{
+			name: "test02",
+			a:    "6.5.102",
+			b:    "6.5.101",
+			want: 1,
+		},
+		{
+			name: "test03",
+			a:    "6.5.100",
+			b:    "6.5.100",
+			want: 0,
+		},
+		{
+			name: "test04",
+			a:    "6.5.100",
+			b:    "6.5",
+			want: -1,
+		},
+		{
+			name: "test05",
+			a:    "6.6.100",
+			b:    "6.5",
+			want: 1,
+		},
+		{
+			name: "test06",
+			a:    "6.6",
+			b:    "6.5",
+			want: 1,
+		},
+		{
+			name: "test07",
+			a:    "6.6",
+			b:    "6.5.100",
+			want: 1,
+		},
+		{
+			name: "test08",
+			a:    "6.5.1232",
+			b:    "6.5-patch.100",
+			want: -1,
+		},
+		{
+			name: "test09",
+			a:    "6.5-patch.1232",
+			b:    "6.5-patch.100",
+			want: 1132,
+		},
+		{
+			name:    "test10",
+			a:       "6",
+			b:       "6.5-patch.100",
+			wantErr: true,
+		},
+		{
+			name:    "test11",
+			a:       "6.",
+			b:       "6.5-patch.100",
+			wantErr: true,
+		},
+		{
+			name:    "test12",
+			a:       "6.a",
+			b:       "6.5-patch.100",
+			wantErr: true,
+		},
+		{
+			name: "test13",
+			a:    "6.5-patch.1232",
+			b:    "6.5-patch.100",
+			want: 1132,
+		},
+		{
+			name: "test14",
+			a:    "6.5-patch.1232",
+			b:    "6.6-patch.100",
+			want: -1,
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := CompareSoftwareVersions(tc.a, tc.b)
+			if tc.wantErr && err == nil {
+				t.Fatal("wantErr but got nil err")
+			}
+			if !tc.wantErr && err != nil {
+				t.Fatalf("don't wantErr but got err (%v)", err)
+			}
+			if tc.want != got {
+				t.Fatalf("want (%d) does not equal got (%d)", tc.want, got)
 			}
 		})
 	}
