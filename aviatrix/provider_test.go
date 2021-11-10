512cda1b42ea93ad2013955b459e35692dbfcbb4
mlin-aviatrix <mlin@aviatrix.com>
2021-11-10 09:31:49 -0800

    Fix lint error

--- aviatrix/provider_test.go
+++ aviatrix/provider_test.go
@@ -2,13 +2,14 @@ package aviatrix
 
 import (
 	"fmt"
-	"github.com/AviatrixSystems/terraform-provider-aviatrix/v2/goaviatrix"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"log"
 	"os"
 	"testing"
 	"time"
+
+	"github.com/AviatrixSystems/terraform-provider-aviatrix/v2/goaviatrix"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
