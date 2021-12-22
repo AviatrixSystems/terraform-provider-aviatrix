a88977b5bbe462b8bac4798c530094fb6edcd54d
Tim Jin <zjin@aviatrix.com>
2021-12-21 17:11:26 -0800

    AVX-17738: Add support of BGP LAN ActiveMesh mode for GCP Transit External Device Conn (#1178)
    * AVX-17738: Add support of enabling bgp lan activemesh for GCP external conn
    
    * AVX-17738: Add support of BGP LAN ActiveMesh mode for GCP Transit External Device Conn
    
    * Update error message
    
    * Address comments

--- goaviatrix/transit_external_device_conn.go
+++ goaviatrix/transit_external_device_conn.go
@@ -43,6 +43,7 @@ type ExternalDeviceConn struct {
 	EnableIkev2            string `form:"enable_ikev2,omitempty"`
 	ManualBGPCidrs         []string
 	TunnelProtocol         string `form:"tunnel_protocol,omitempty"`
+	EnableBgpLanActiveMesh bool   `form:"bgp_lan_activemesh,omitempty"`
 	PeerVnetId             string `form:"peer_vnet_id,omitempty"`
 	RemoteLanIP            string `form:"remote_lan_ip,omitempty"`
 	LocalLanIP             string `form:"local_lan_ip,omitempty"`
@@ -60,6 +61,7 @@ type EditExternalDeviceConnDetail struct {
 	BgpLocalAsNum          string        `json:"bgp_local_asn_number,omitempty"`
 	BgpRemoteAsNum         string        `json:"bgp_remote_asn_number,omitempty"`
 	BgpStatus              string        `json:"bgp_status,omitempty"`
+	EnableBgpLanActiveMesh bool          `json:"bgp_lan_activemesh,omitempty"`
 	RemoteGatewayIP        string        `json:"peer_ip,omitempty"`
 	RemoteSubnet           string        `json:"remote_cidr,omitempty"`
 	DirectConnect          bool          `json:"direct_connect_primary,omitempty"`
@@ -242,7 +244,8 @@ func (c *Client) GetExternalDeviceConnDetail(externalDeviceConn *ExternalDeviceC
 				}
 			}
 		} else {
-			if len(externalDeviceConnDetail.Tunnels) == 2 {
+			externalDeviceConn.EnableBgpLanActiveMesh = externalDeviceConnDetail.EnableBgpLanActiveMesh
+			if len(externalDeviceConnDetail.Tunnels) == 2 || len(externalDeviceConnDetail.Tunnels) == 4 {
 				externalDeviceConn.HAEnabled = "enabled"
 				externalDeviceConn.BackupBgpRemoteAsNum = backupBgpRemoteAsNumber
 				externalDeviceConn.BackupRemoteLanIP = externalDeviceConnDetail.BackupRemoteLanIP
