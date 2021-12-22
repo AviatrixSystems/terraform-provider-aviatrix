7c4a6ee42771ee4b7ba8b1f1e97d3d9d4c96527b
Shuo Liu <sliu@aviatrix.com>
2021-12-22 10:26:14 -0800

    AVX-17663: add workaround for EOF error when deleting HA gateway (#1177)
    * AVX-17663: add workaround for EOF error when deleting HA gateway
    
    * break if HA is not found

--- aviatrix/resource_aviatrix_transit_gateway.go
+++ aviatrix/resource_aviatrix_transit_gateway.go
@@ -3096,9 +3096,29 @@ func resourceAviatrixTransitGatewayDelete(d *schema.ResourceData, meta interface
 	if haSubnet != "" || haZone != "" {
 		gateway.GwName += "-hagw"
 
-		err := client.DeleteGateway(gateway)
-		if err != nil {
-			return fmt.Errorf("failed to delete Aviatrix Transit Gateway HA gateway: %s", err)
+		try, maxTries, backoff := 0, 2, 500*time.Millisecond
+
+		for {
+			try++
+			err := client.DeleteGateway(gateway)
+			if err != nil {
+				if strings.Contains(err.Error(), "not found") {
+					break
+				}
+
+				if !strings.Contains(err.Error(), "EOF") {
+					return fmt.Errorf("failed to delete Aviatrix Transit Gateway HA gateway: %s", err)
+				}
+			} else {
+				break
+			}
+
+			if try == maxTries {
+				return fmt.Errorf("failed to delete Aviatrix Transit Gateway HA gateway: %s", err)
+			}
+			time.Sleep(backoff)
+			// Double the backoff time after each failed try
+			backoff *= 2
 		}
 	}
 
