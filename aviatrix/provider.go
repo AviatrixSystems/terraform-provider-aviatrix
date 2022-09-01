ae0cfd22b69d83f678eaeb995852167bb165df76
zhiqiangjin <zjin@aviatrix.com>
2022-09-01 16:36:00 -0700

    Update supported controller version to UserConnect-6.9

--- aviatrix/provider.go
+++ aviatrix/provider.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-var supportedVersions = []string{"6.8"}
+var supportedVersions = []string{"6.9"}
 
 // Provider returns a schema.Provider for Aviatrix.
 func Provider() *schema.Provider {
