c295d625ab90982175eb7e0e0d7d8c529a8c5937
Shuo Liu <sliu@aviatrix.com>
2023-02-13 09:51:17 -0800

    AVX-36072: enable the order of policies in traffic_classifier

--- goaviatrix/traffic_classifier.go
+++ goaviatrix/traffic_classifier.go
@@ -2,10 +2,6 @@ package goaviatrix
 
 import (
 	"context"
-	"reflect"
-	"sort"
-
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 type PortRange struct {
@@ -59,77 +55,4 @@ func (c *Client) GetTrafficClassifier(ctx context.Context) (*[]PolicyList, error
 func (c *Client) DeleteTrafficClassifier(ctx context.Context) error {
 	endpoint := "ipsla/traffic-classifier"
 	return c.DeleteAPIContext25(ctx, endpoint, nil)
-}
-
-func DiffSuppressFuncTrafficClassifier(k, old, new string, d *schema.ResourceData) bool {
-	pOld, pNew := d.GetChange("policies")
-	var policiesOld []map[string]interface{}
-
-	for _, p0 := range pOld.([]interface{}) {
-		p1 := p0.(map[string]interface{})
-		p2 := make(map[string]interface{})
-
-		for k, v := range p1 {
-			if k != "uuid" && k != "port_ranges" {
-				p2[k] = v
-			}
-		}
-
-		pr := p1["port_ranges"].(*schema.Set).List()
-		var portRanges []map[string]interface{}
-		for _, v := range pr {
-			temp := make(map[string]interface{})
-			temp["lo"] = v.(map[string]interface{})["lo"]
-			temp["hi"] = v.(map[string]interface{})["hi"]
-			portRanges = append(portRanges, temp)
-		}
-
-		sort.Slice(portRanges, func(i, j int) bool {
-			return portRanges[i]["lo"].(string) < portRanges[j]["lo"].(string)
-		})
-
-		p2["port_ranges"] = portRanges
-
-		policiesOld = append(policiesOld, p2)
-	}
-
-	var policiesNew []map[string]interface{}
-
-	for _, p0 := range pNew.([]interface{}) {
-		p1 := p0.(map[string]interface{})
-		p2 := make(map[string]interface{})
-
-		for k, v := range p1 {
-			if k != "uuid" && k != "port_ranges" {
-				p2[k] = v
-			}
-		}
-
-		pr := p1["port_ranges"].(*schema.Set).List()
-		var portRanges []map[string]interface{}
-		for _, v := range pr {
-			temp := make(map[string]interface{})
-			temp["lo"] = v.(map[string]interface{})["lo"]
-			temp["hi"] = v.(map[string]interface{})["hi"]
-			portRanges = append(portRanges, temp)
-		}
-
-		sort.Slice(portRanges, func(i, j int) bool {
-			return portRanges[i]["lo"].(string) < portRanges[j]["lo"].(string)
-		})
-
-		p2["port_ranges"] = portRanges
-
-		policiesNew = append(policiesNew, p2)
-	}
-
-	sort.Slice(policiesOld, func(i, j int) bool {
-		return policiesOld[i]["name"].(string) < policiesOld[j]["name"].(string)
-	})
-
-	sort.Slice(policiesNew, func(i, j int) bool {
-		return policiesNew[i]["name"].(string) < policiesNew[j]["name"].(string)
-	})
-
-	return reflect.DeepEqual(policiesOld, policiesNew)
 }
