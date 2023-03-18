9fa1d6ec4c39160354b029dd660a30185193a8fb
zhiqiangjin <zjin@aviatrix.com>
2023-03-17 20:14:29 -0700

    Cover delete_stateful_firewall_rules as well

--- goaviatrix/firewall.go
+++ goaviatrix/firewall.go
@@ -156,7 +156,7 @@ func (c *Client) DeleteFirewallPolicy(fw *Firewall) error {
 		return fmt.Errorf("could not marshal firewall policies: %v", err)
 	}
 
-	form := map[string]string{
+	form := map[string]interface{}{
 		"CID":          c.CID,
 		"action":       "delete_stateful_firewall_rules",
 		"gateway_name": fw.GwName,
@@ -174,7 +174,7 @@ func (c *Client) DeleteFirewallPolicy(fw *Firewall) error {
 		return nil
 	}
 
-	return c.PostAPI(form["action"], form, checkFunc)
+	return c.PostAPIContext2(context.Background(), nil, form["action"].(string), form, checkFunc)
 }
 
 func (c *Client) GetFirewallPolicy(fw *Firewall) (*Firewall, error) {
