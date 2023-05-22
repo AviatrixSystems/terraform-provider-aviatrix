ffa667eed9a53a704c169e0fc4627b0f025fca29
zhiqiangjin <zjin@aviatrix.com>
2023-05-22 13:30:52 -0700

    Add computed attributes

--- aviatrix/resource_aviatrix_distributed_firewalling_proxy_ca_config.go
+++ aviatrix/resource_aviatrix_distributed_firewalling_proxy_ca_config.go
@@ -2,7 +2,7 @@ package aviatrix
 
 import (
 	"context"
-	"log"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 
@@ -14,7 +14,7 @@ func resourceAviatrixDistributedFirewallingProxyCaConfig() *schema.Resource {
 	return &schema.Resource{
 		CreateWithoutTimeout: resourceAviatrixDistributedFirewallingProxyCaConfigCreate,
 		ReadWithoutTimeout:   resourceAviatrixDistributedFirewallingProxyCaConfigRead,
-		UpdateWithoutTimeout: resourceAviatrixDistributedFirewallingProxyCaConfigUpdate,
+		//UpdateWithoutTimeout: resourceAviatrixDistributedFirewallingProxyCaConfigUpdate,
 		DeleteWithoutTimeout: resourceAviatrixDistributedFirewallingProxyCaConfigDelete,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
@@ -33,6 +33,36 @@ func resourceAviatrixDistributedFirewallingProxyCaConfig() *schema.Resource {
 				ForceNew:    true,
 				Description: "",
 			},
+			"unique_serial": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Unique serial of created cert.",
+			},
+			"issuer_name": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Issuer name of created cert.",
+			},
+			"common_name": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Common name of created cert.",
+			},
+			"expiration_time": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Expiration time of created cert.",
+			},
+			"sans": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Expiration time of created cert.",
+			},
+			"upload_info": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Expiration time of created cert.",
+			},
 		},
 	}
 }
@@ -40,112 +70,44 @@ func resourceAviatrixDistributedFirewallingProxyCaConfig() *schema.Resource {
 func resourceAviatrixDistributedFirewallingProxyCaConfigCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	client := meta.(*goaviatrix.Client)
 
-	for _, v := range d.Get("ca_certificates").(*schema.Set).List() {
-		certInstance := v.(map[string]interface{})
-		s2cCaCert := &goaviatrix.S2CCaCert{
-			TagName:       d.Get("tag_name").(string),
-			CaCertificate: certInstance["cert_content"].(string),
-		}
-
-		if err := client.CreateS2CCaCert(ctx, s2cCaCert); err != nil {
-			return diag.Errorf("failed to create s2c ca cert tag: %v", err)
-		}
+	proxyCaConfig := &goaviatrix.ProxyCaConfig{
+		CaCert: d.Get("ca_cert").(string),
+		CaKey:  d.Get("ca_key").(string),
 	}
 
-	d.SetId(d.Get("tag_name").(string))
-	return resourceAviatrixSite2CloudCaCertTagRead(ctx, d, meta)
+	if err := client.SetNewCertificate(ctx, proxyCaConfig); err != nil {
+		return diag.Errorf("failed to create s2c ca cert tag: %v", err)
+	}
+
+	d.SetId(strings.Replace(client.ControllerIP, ".", "-", -1))
+	return resourceAviatrixDistributedFirewallingProxyCaConfigRead(ctx, d, meta)
 }
 
 func resourceAviatrixDistributedFirewallingProxyCaConfigRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	client := meta.(*goaviatrix.Client)
 
-	tagName := d.Get("tag_name").(string)
-
-	if tagName == "" {
-		id := d.Id()
-		log.Printf("[DEBUG] Looks like an import. Import Id is %s", id)
-		d.Set("tag_name", id)
-		d.SetId(id)
+	if d.Id() != strings.Replace(client.ControllerIP, ".", "-", -1) {
+		return diag.Errorf("ID: %s does not match controller IP. Please provide correct ID for importing", d.Id())
 	}
 
-	s2cCaCertTag := &goaviatrix.S2CCaCertTag{
-		TagName: d.Get("tag_name").(string),
-	}
-
-	s2cCaCertTagResp, err := client.GetS2CCaCertTag(ctx, s2cCaCertTag)
+	proxyCaConfig, err := client.GetCaCertificate(ctx)
 	if err != nil {
 		if err == goaviatrix.ErrNotFound {
 			d.SetId("")
 			return nil
 		}
-		return diag.Errorf("couldn't get site2cloud ca cert tag: %s", err)
+		return diag.Errorf("failed to read controller access allow list config: %s", err)
 	}
 
-	d.Set("tag_name", s2cCaCertTagResp.TagName)
+	d.Set("unique_serial", proxyCaConfig.SerialNumber)
+	d.Set("issuer_name", proxyCaConfig.Issuer)
+	d.Set("common_name", proxyCaConfig.CommonName)
+	d.Set("expiration_time", proxyCaConfig.ExpirationDate)
+	d.Set("sans", proxyCaConfig.SANs)
+	d.Set("upload_info", proxyCaConfig.UploadInfo)
 
-	var caCertInstances []map[string]interface{}
-	for _, certInstance := range s2cCaCertTagResp.CaCertificates {
-		instanceInfo := make(map[string]interface{})
-		instanceInfo["cert_content"] = certInstance.CertContent
-		instanceInfo["id"] = certInstance.ID
-		instanceInfo["unique_serial"] = certInstance.SerialNumber
-		instanceInfo["issuer_name"] = certInstance.Issuer
-		instanceInfo["common_name"] = certInstance.CommonName
-		instanceInfo["expiration_time"] = certInstance.ExpirationDate
-
-		caCertInstances = append(caCertInstances, instanceInfo)
-	}
-	if err := d.Set("ca_certificates", caCertInstances); err != nil {
-		log.Printf("[WARN] Error setting 'ca_certificates' for (%s): %s", d.Id(), err)
-	}
-
-	d.SetId(s2cCaCertTagResp.TagName)
+	d.SetId(strings.Replace(client.ControllerIP, ".", "-", -1))
 	return nil
-}
-
-func resourceAviatrixDistributedFirewallingProxyCaConfigUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	client := meta.(*goaviatrix.Client)
-	d.Partial(true)
-
-	if d.HasChange("ca_certificates") {
-		oldCerts, newCerts := d.GetChange("ca_certificates")
-
-		mapCaCertID := make(map[string]bool)
-		for _, cert := range oldCerts.(*schema.Set).List() {
-			certInstance := cert.(map[string]interface{})
-			mapCaCertID[certInstance["id"].(string)] = true
-		}
-
-		for _, cert := range newCerts.(*schema.Set).List() {
-			certInstance := cert.(map[string]interface{})
-			if certInstance["id"].(string) == "" {
-				s2cCaCert := &goaviatrix.S2CCaCert{
-					TagName:       d.Get("tag_name").(string),
-					CaCertificate: certInstance["cert_content"].(string),
-				}
-
-				if err := client.CreateS2CCaCert(ctx, s2cCaCert); err != nil {
-					return diag.Errorf("failed to create s2c ca cert in update: %v", err)
-				}
-				continue
-			}
-			delete(mapCaCertID, certInstance["id"].(string))
-		}
-
-		for id := range mapCaCertID {
-			cert := &goaviatrix.CaCertInstance{
-				ID: id,
-			}
-
-			if err := client.DeleteCertInstance(ctx, cert); err != nil {
-				return diag.Errorf("failed to delete ca cert %s in update: %s", cert.ID, err)
-			}
-		}
-	}
-
-	d.Partial(false)
-	d.SetId(d.Get("tag_name").(string))
-	return resourceAviatrixSite2CloudCaCertTagRead(ctx, d, meta)
 }
 
 func resourceAviatrixDistributedFirewallingProxyCaConfigDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
--- goaviatrix/distributed_firewalling_proxy_ca_config.go
+++ goaviatrix/distributed_firewalling_proxy_ca_config.go
@@ -6,33 +6,34 @@ import (
 )
 
 type ProxyCaConfig struct {
-	CaCert string `json:"cert,omitempty"`
-	CaKey string `json:"ca_key,omitempty"`
+	CaCert         string `json:"cert,omitempty"`
+	CaKey          string `json:"ca_key,omitempty"`
+	SerialNumber   string `json:"serial_number,omitempty"`
+	Issuer         string `json:"issuer,omitempty"`
+	CommonName     string `json:"common_name,omitempty"`
+	ExpirationDate string `json:"expire_date,omitempty"`
+	SANs           string `json:"SANs,omitempty"`
+	UploadInfo     string `json:"upload_info,omitempty"`
 }
 
-func (c *Client) SetEnforcementLevel(ctx context.Context, proxyCaConfig *ProxyCaConfig) error {
+func (c *Client) SetNewCertificate(ctx context.Context, proxyCaConfig *ProxyCaConfig) error {
 	endpoint := fmt.Sprintf("mitm/ca")
 	return c.PutAPIContext25(ctx, endpoint, proxyCaConfig)
 }
 
-//func (c *Client) GetEnforcementLevel(ctx context.Context) (*ProxyCaConfig, error) {
-//	endpoint := "mitm/origin-cert-verify-2"
-//
-//	var data EnforcementLevel
-//	err := c.GetAPIContext25(ctx, &data, endpoint, nil)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	return &data, nil
-//}
-//
-//func (c *Client) UpdateEnforcementLevel(ctx context.Context, enforcementLevel *ProxyCaConfig) error {
-//	endpoint := fmt.Sprintf("mitm/origin-cert-verify?level=%s", enforcementLevel.Level)
-//	return c.PutAPIContext25(ctx, endpoint, enforcementLevel)
-//}
-//
-//func (c *Client) DeleteEnforcementLevel(ctx context.Context) error {
-//	endpoint := "mitm/origin-cert-verify?level=PERMISSIVE"
-//	return c.PutAPIContext25(ctx, endpoint, nil)
-//}
+func (c *Client) GetCaCertificate(ctx context.Context) (*ProxyCaConfig, error) {
+	endpoint := "mitm/ca?info"
+
+	var data ProxyCaConfig
+	err := c.GetAPIContext25(ctx, &data, endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &data, nil
+}
+
+func (c *Client) DeleteEnforcementLevel(ctx context.Context) error {
+	endpoint := "mitm/ca"
+	return c.PutAPIContext25(ctx, endpoint, nil)
+}
