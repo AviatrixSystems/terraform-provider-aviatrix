c7c674ec241e4f94ae6a97f00dce979f88a70602
Tim Jin <zjin@aviatrix.com>
2023-09-22 09:20:41 -0700

    AVX-45450: add tunnel_count support to aviatrix_spoke_transit_attachment (#1868)
    * AVX-45450: add tunnel_count support to aviatrix_spoke_transit_attachment
    
    * Fix lint check
    
    * Fix lint check

--- aviatrix/resource_aviatrix_spoke_transit_attachment_test.go
+++ aviatrix/resource_aviatrix_spoke_transit_attachment_test.go
@@ -36,6 +36,7 @@ func TestAccAviatrixSpokeTransitAttachment_basic(t *testing.T) {
 					testAccCheckSpokeTransitAttachmentExists(resourceName, &spokeTransitAttachment),
 					resource.TestCheckResourceAttr(resourceName, "spoke_gw_name", fmt.Sprintf("tfs-%s", rName)),
 					resource.TestCheckResourceAttr(resourceName, "transit_gw_name", fmt.Sprintf("tft-%s", rName)),
+					resource.TestCheckResourceAttr(resourceName, "tunnel_count", "4"),
 				),
 			},
 			{
@@ -50,38 +51,57 @@ func TestAccAviatrixSpokeTransitAttachment_basic(t *testing.T) {
 func testAccSpokeTransitAttachmentConfigBasic(rName string) string {
 	return fmt.Sprintf(`
 resource "aviatrix_account" "test" {
-	account_name       = "tfa-%s"
 	cloud_type         = 1
+	account_name       = "tfa-%s"
 	aws_account_number = "%s"
 	aws_iam            = false
 	aws_access_key     = "%s"
 	aws_secret_key     = "%s"
 }
-resource "aviatrix_spoke_gateway" "test"{
-	cloud_type   = 1
-	account_name = aviatrix_account.test.account_name
-	gw_name      = "tfs-%s"
-	vpc_id       = "%s"
-	vpc_reg      = "%s"
-	gw_size      = "t2.micro"
-	subnet       = "%s"
+resource "aviatrix_vpc" "test" {
+	cloud_type           = 1
+	account_name         = aviatrix_account.test.account_name
+	region               = "us-west-1"
+	name                 = "aws-vpc-test-0"
+	cidr                 = "16.0.0.0/20"
+	aviatrix_transit_vpc = true
 }
 resource "aviatrix_transit_gateway" "test" {
+	cloud_type     = 1
+	account_name   = aviatrix_account.test.account_name
+	gw_name        = "tft-%s"
+	vpc_id         = aviatrix_vpc.test.vpc_id
+	vpc_reg        = aviatrix_vpc.test.region
+	gw_size        = "c5.xlarge"
+	insane_mode    = true
+	subnet         = join(".", [join(".", slice(split(".", aviatrix_vpc.test.public_subnets[0].cidr), 0, 3)), "128/26"]) #"16.0.0.128/26"
+	insane_mode_az = "us-west-1b"
+}
+resource "aviatrix_vpc" "test1" {
 	cloud_type   = 1
 	account_name = aviatrix_account.test.account_name
-	gw_name      = "tft-%s"
-	vpc_id       = "%s"
-	vpc_reg      = "%s"
-	gw_size      = "t2.micro"
-	subnet       = "%s"
+	cidr         = "173.31.0.0/20"
+	name         = "aws-vpc-test-1"
+	region       = "us-east-1"
+}
+resource "aviatrix_spoke_gateway" "test" {
+	cloud_type     = 1
+	account_name   = aviatrix_account.test.account_name
+	gw_name        = "tfs-%s"
+	vpc_id         = aviatrix_vpc.test1.vpc_id
+	vpc_reg        = aviatrix_vpc.test1.region
+	gw_size        = "c5.xlarge"
+	insane_mode    = true
+	subnet         = join(".", [join(".", slice(split(".", aviatrix_vpc.test1.public_subnets[1].cidr), 0, 2)), "12.0/26"]) #"173.31.12.0/26"
+	insane_mode_az = "us-east-1a"
 }
 resource "aviatrix_spoke_transit_attachment" "test" {
-	spoke_gw_name   = aviatrix_spoke_gateway.test.gw_name
-	transit_gw_name = aviatrix_transit_gateway.test.gw_name
+	spoke_gw_name   = aviatrix_spoke_gateway.test.id
+	transit_gw_name = aviatrix_transit_gateway.test.id
+	tunnel_count    = 4
 }
 	`, rName, os.Getenv("AWS_ACCOUNT_NUMBER"), os.Getenv("AWS_ACCESS_KEY"), os.Getenv("AWS_SECRET_KEY"),
-		rName, os.Getenv("AWS_VPC_ID"), os.Getenv("AWS_REGION"), os.Getenv("AWS_SUBNET"),
-		rName, os.Getenv("AWS_VPC_ID2"), os.Getenv("AWS_REGION2"), os.Getenv("AWS_SUBNET2"))
+		rName, rName)
 }
 
 func testAccCheckSpokeTransitAttachmentExists(n string, spokeTransitAttachment *goaviatrix.SpokeTransitAttachment) resource.TestCheckFunc {
