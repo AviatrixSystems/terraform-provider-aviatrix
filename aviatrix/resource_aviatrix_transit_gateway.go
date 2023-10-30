2b3ca2b44a2d363cd85c04e022e3c0c992502c2e
Tim Jin <zjin@aviatrix.com>
2023-10-30 14:02:26 -0700

    AVX-47370: Fix ha_private_mode_subnet_zone update issue for private mode (#1891)

--- aviatrix/resource_aviatrix_transit_gateway.go
+++ aviatrix/resource_aviatrix_transit_gateway.go
@@ -1200,6 +1200,9 @@ func resourceAviatrixTransitGatewayCreate(d *schema.ResourceData, meta interface
 
 		if privateModeInfo.EnablePrivateMode {
 			haPrivateModeSubnetZone := d.Get("ha_private_mode_subnet_zone").(string)
+			if haPrivateModeSubnetZone == "" && goaviatrix.IsCloudType(gateway.CloudType, goaviatrix.AWSRelatedCloudTypes) {
+				return fmt.Errorf("%q must be set when creating a Transit HA Gateway in AWS with Private Mode enabled on the Controller", "ha_private_mode_subnet_zone")
+			}
 			transitHaGw.Subnet = haSubnet + "~~" + haPrivateModeSubnetZone
 		}
 
@@ -1739,7 +1742,7 @@ func resourceAviatrixTransitGatewayRead(d *schema.ResourceData, meta interface{}
 	}
 
 	if _, zoneIsSet := d.GetOk("zone"); goaviatrix.IsCloudType(gw.CloudType, goaviatrix.AzureArmRelatedCloudTypes) && (isImport || zoneIsSet) &&
-		gw.GatewayZone != "AvailabilitySet" {
+		gw.GatewayZone != "AvailabilitySet" && gw.LbVpcId == "" {
 		d.Set("zone", "az-"+gw.GatewayZone)
 	}
 	d.Set("enable_vpc_dns_server", goaviatrix.IsCloudType(gw.CloudType, goaviatrix.AWSRelatedCloudTypes|goaviatrix.AzureArmRelatedCloudTypes|goaviatrix.AliCloudRelatedCloudTypes) && gw.EnableVpcDnsServer == "Enabled")
@@ -1910,8 +1913,12 @@ func resourceAviatrixTransitGatewayRead(d *schema.ResourceData, meta interface{}
 	}
 
 	d.Set("private_mode_lb_vpc_id", gw.LbVpcId)
-	if gw.LbVpcId != "" && goaviatrix.IsCloudType(gw.CloudType, goaviatrix.AWSRelatedCloudTypes) {
-		d.Set("private_mode_subnet_zone", gw.GatewayZone)
+	if gw.LbVpcId != "" && gw.GatewayZone != "AvailabilitySet" {
+		if goaviatrix.IsCloudType(gw.CloudType, goaviatrix.AWSRelatedCloudTypes) {
+			d.Set("private_mode_subnet_zone", gw.GatewayZone)
+		} else if goaviatrix.IsCloudType(gw.CloudType, goaviatrix.AzureArmRelatedCloudTypes) {
+			d.Set("private_mode_subnet_zone", "az-"+gw.GatewayZone)
+		}
 	} else {
 		d.Set("private_mode_subnet_zone", nil)
 	}
@@ -1947,7 +1954,7 @@ func resourceAviatrixTransitGatewayRead(d *schema.ResourceData, meta interface{}
 	if goaviatrix.IsCloudType(gw.HaGw.CloudType, goaviatrix.AWSRelatedCloudTypes|goaviatrix.AzureArmRelatedCloudTypes|goaviatrix.OCIRelatedCloudTypes|goaviatrix.AliCloudRelatedCloudTypes) {
 		d.Set("ha_subnet", gw.HaGw.VpcNet)
 		if zone := d.Get("ha_zone"); goaviatrix.IsCloudType(gw.HaGw.CloudType, goaviatrix.AzureArmRelatedCloudTypes) && (isImport || zone.(string) != "") {
-			if gw.HaGw.GatewayZone != "AvailabilitySet" {
+			if gw.LbVpcId == "" && gw.HaGw.GatewayZone != "AvailabilitySet" {
 				d.Set("ha_zone", "az-"+gw.HaGw.GatewayZone)
 			} else {
 				d.Set("ha_zone", "")
@@ -1991,8 +1998,12 @@ func resourceAviatrixTransitGatewayRead(d *schema.ResourceData, meta interface{}
 		d.Set("ha_oob_availability_zone", gw.HaGw.GatewayZone)
 	}
 
-	if gw.LbVpcId != "" && goaviatrix.IsCloudType(gw.HaGw.CloudType, goaviatrix.AWSRelatedCloudTypes) {
-		d.Set("ha_private_mode_subnet_zone", gw.HaGw.GatewayZone)
+	if gw.LbVpcId != "" && gw.GatewayZone != "AvailabilitySet" {
+		if goaviatrix.IsCloudType(gw.CloudType, goaviatrix.AWSRelatedCloudTypes) {
+			d.Set("ha_private_mode_subnet_zone", gw.HaGw.GatewayZone)
+		} else if goaviatrix.IsCloudType(gw.CloudType, goaviatrix.AzureArmRelatedCloudTypes) {
+			d.Set("ha_private_mode_subnet_zone", "az-"+gw.HaGw.GatewayZone)
+		}
 	} else {
 		d.Set("ha_private_mode_subnet_zone", "")
 	}
@@ -2295,8 +2306,8 @@ func resourceAviatrixTransitGatewayUpdate(d *schema.ResourceData, meta interface
 
 		if privateModeInfo.EnablePrivateMode {
 			if newHaGwEnabled || changeHaGw {
-				if _, ok := d.GetOk("ha_private_mode_subnet_zone"); !ok {
-					return fmt.Errorf("%q is required if private mode is enabled and %q is provided", "ha_private_mode_subnet_zone", "ha_subnet")
+				if _, ok := d.GetOk("ha_private_mode_subnet_zone"); !ok && goaviatrix.IsCloudType(gateway.CloudType, goaviatrix.AWSRelatedCloudTypes) {
+					return fmt.Errorf("%q is required when creating a Transit HA Gateway in AWS if private mode is enabled and %q is provided", "ha_private_mode_subnet_zone", "ha_subnet")
 				}
 
 				privateModeSubnetZone := d.Get("ha_private_mode_subnet_zone").(string)
