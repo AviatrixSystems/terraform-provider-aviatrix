0a6d5c74d90c86d5644477eee8f2ec7c15bcc7e2
Robert Panzer <rpanzer@aviatrix.com>
2024-06-20 12:14:46 +0200

    Add support for debugging provider

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/AviatrixSystems/terraform-provider-aviatrix/v3/aviatrix"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{
+		Debug:        debug,
 		ProviderFunc: aviatrix.Provider,
-	})
+		ProviderAddr: "AviatrixSystems/aviatrix",
+	}
+
+	plugin.Serve(opts)
 }
