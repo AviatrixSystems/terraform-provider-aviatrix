c8c69cf6dcd4c585136cb659a4aa4ca6e7a827e5
Sailee Rane <srane@avaitrix.com>
2024-06-28 14:41:14 -0700

    Deprecating bandwidth attribute from interface

--- aviatrix/resource_aviatrix_edge_equinix.go
+++ aviatrix/resource_aviatrix_edge_equinix.go
@@ -215,6 +215,7 @@ func resourceAviatrixEdgeEquinix() *schema.Resource {
 							Type:        schema.TypeInt,
 							Optional:    true,
 							Description: "The rate of data can be moved through the interface, requires an integer value. Unit is in Mb/s.",
+							Deprecated:  "Bandwidth will be removed in a future release.",
 						},
 						"enable_dhcp": {
 							Type:        schema.TypeBool,
