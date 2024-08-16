a92b3b78947ccd59436f58962a5ce2f4c12e6326
Susan Hinrichs <shinrichs@aviatrix.com>
2024-08-16 20:14:08 +0000

    goimport

--- goaviatrix/utils_test.go
+++ goaviatrix/utils_test.go
@@ -1,8 +1,10 @@
 package goaviatrix
 
-import "testing"
+import (
+	"testing"
 
-import "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestValidateASN(t *testing.T) {
 	tt := []struct {
