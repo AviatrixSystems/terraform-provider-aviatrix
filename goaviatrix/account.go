be18e5e6828539004fcf2d65baf0b48fd114d2f5
Daniel Nardo <dnardo@aviatrix.com>
2024-08-22 13:04:07 -0400

    AVX-56220: Invalidate account cache after CRUD operations (#2025) [Backport-UserConnect-7.2] (#2026)
    After create, delete or update, invalidate the cache.

--- goaviatrix/account.go
+++ goaviatrix/account.go
@@ -218,6 +218,12 @@ func (c *Client) CreateAWSSAccount(account *Account) error {
 	return c.PostFileAPI(params, files, DuplicateBasicCheck)
 }
 
+func (c *Client) InvalidateCache() {
+	c.cacheMutex.Lock()
+	defer c.cacheMutex.Unlock()
+	c.cachedAccounts = nil
+}
+
 func (c *Client) ListAccounts() ([]Account, error) {
 	// If cached accounts are recent enough, return them
 	c.cacheMutex.Lock()
