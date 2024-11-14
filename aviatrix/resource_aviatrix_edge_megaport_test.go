1c4a61aae1f6aaf165dccb2ecd945bd96ed236d5
Sailee Rane <srane@avaitrix.com>
2024-11-14 11:39:34 -0800

    Adding test for edge_megaport spoke

---
+++ aviatrix/resource_aviatrix_edge_megaport_test.go
@@ -0,0 +1,136 @@
+package aviatrix
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+
+	"github.com/AviatrixSystems/terraform-provider-aviatrix/v3/goaviatrix"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccAviatrixEdgeMegaport_basic(t *testing.T) {
+	if os.Getenv("SKIP_EDGE_Megaport") == "yes" {
+		t.Skip("Skipping Edge Megaport test as SKIP_EDGE_Megaport is set")
+	}
+
+	resourceName := "aviatrix_edge_Megaport.test"
+	accountName := "acc-" + acctest.RandString(5)
+	edgeMegaportUsername := "megaport-user-" + acctest.RandString(5)
+	gwName := "gw-" + acctest.RandString(5)
+	siteId := "site-" + acctest.RandString(5)
+	path, _ := os.Getwd()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckEdgeMegaportDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccEdgeMegaportBasic(accountName, edgeMegaportUsername, gwName, siteId, path),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckEdgeMegaportExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "gw_name", gwName),
+					resource.TestCheckResourceAttr(resourceName, "site_id", siteId),
+					resource.TestCheckResourceAttr(resourceName, "interfaces.0.ip_address", "10.230.5.32/24"),
+					resource.TestCheckResourceAttr(resourceName, "interfaces.1.ip_address", "10.230.3.32/24"),
+					resource.TestCheckResourceAttr(resourceName, "interfaces.2.ip_address", "172.16.15.162/20"),
+					resource.TestCheckResourceAttr(resourceName, "bgp_polling_time", "50"),
+					resource.TestCheckResourceAttr(resourceName, "bgp_neighbor_status_polling_time", "5"),
+				),
+			},
+			{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"ztp_file_download_path"},
+			},
+		},
+	})
+}
+
+func testAccEdgeMegaportBasic(edgeMegaportUsername, accountName, gwName, siteId, path string) string {
+	return fmt.Sprintf(`
+resource "aviatrix_account" "test" {
+	account_name          = "%s"
+	cloud_type            = 524288
+	edge_Megaport_username = "%s"
+}
+resource "aviatrix_edge_Megaport" "test" {
+	account_name                       = aviatrix_account.test_account.account_name
+	gw_name                            = "%s"
+	site_id                            = "%s"
+	ztp_file_download_path             = "%s"
+	bgp_polling_time                   = 50
+	bgp_neighbor_status_polling_time   = 5
+	
+	interfaces {
+		name          = "eth0"
+		type          = "WAN"
+		ip_address    = "10.230.5.32/24"
+		gateway_ip    = "10.230.5.100"
+		wan_public_ip = "64.71.24.221"
+	}
+	
+	interfaces {
+		name       = "eth1"
+		type       = "LAN"
+		ip_address = "10.230.3.32/24"
+	}
+	
+	interfaces {
+		name        = "eth2"
+		type        = "MANAGEMENT"
+		enable_dhcp = false
+		ip_address  = "172.16.15.162/20"
+		gateway_ip  = "172.16.0.1"
+	}
+}
+ `, accountName, edgeMegaportUsername, gwName, siteId, path)
+}
+
+func testAccCheckEdgeMegaportExists(resourceName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("edge megaport not found: %s", resourceName)
+		}
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("no edge megaport id is set")
+		}
+
+		client := testAccProvider.Meta().(*goaviatrix.Client)
+
+		edgeSpoke, err := client.GetEdgeMegaport(context.Background(), rs.Primary.Attributes["gw_name"])
+		if err != nil {
+			return err
+		}
+		if edgeSpoke.GwName != rs.Primary.ID {
+			return fmt.Errorf("could not find edge megaport")
+		}
+		return nil
+	}
+}
+
+func testAccCheckEdgeMegaportDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*goaviatrix.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aviatrix_edge_megaport" {
+			continue
+		}
+
+		_, err := client.GetEdgeMegaport(context.Background(), rs.Primary.Attributes["gw_name"])
+		if err != goaviatrix.ErrNotFound {
+			return fmt.Errorf("edge megaport still exists")
+		}
+	}
+
+	return nil
+}
