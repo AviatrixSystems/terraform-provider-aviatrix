fa2d28305c9dc26e8c7b4ea25a2fb9cc9f3a44ee
Sailee Rane <srane@avaitrix.com>
2024-11-26 14:15:50 -0800

    Updating the connection status for edge and transit

--- aviatrix/resource_aviatrix_transit_external_device_conn.go
+++ aviatrix/resource_aviatrix_transit_external_device_conn.go
@@ -1089,13 +1089,12 @@ func resourceAviatrixTransitExternalDeviceConnRead(d *schema.ResourceData, meta 
 		}
 
 		d.Set("enable_bfd", conn.EnableBfd)
-		if conn.EnableBfd {
+		// set the bgp_bfd config details only if the user has enabled BFD and provided the config details. For default values, the config is not set
+		bgpBfdConfig := d.Get("bgp_bfd").([]interface{})
+		if conn.EnableBfd && len(bgpBfdConfig) != 0 {
 			var bgpBfdConfig []map[string]interface{}
 			bfd := conn.BgpBfdConfig
 			bfdMap := make(map[string]interface{})
-			bfdMap["transmit_interval"] = defaultBfdTransmitInterval
-			bfdMap["receive_interval"] = defaultBfdReceiveInterval
-			bfdMap["multiplier"] = defaultBfdMultiplier
 			if bfd.TransmitInterval != 0 {
 				bfdMap["transmit_interval"] = bfd.TransmitInterval
 			}
@@ -1364,6 +1363,9 @@ func resourceAviatrixTransitExternalDeviceConnUpdate(d *schema.ResourceData, met
 			}
 		} else {
 			// bgp bfd is disabled
+			if len(bgpBfdConfig) > 0 {
+				return fmt.Errorf("bgp_bfd config can't be set when BFD is disabled")
+			}
 			externalDeviceConn := &goaviatrix.ExternalDeviceConn{
 				GwName:         d.Get("gw_name").(string),
 				ConnectionName: d.Get("connection_name").(string),
