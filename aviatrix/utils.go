1e9b6d5f3045e9dc6b646e7d3f2d3e8cfbba9208
leiav <lfu@aviatrix.com>
2024-12-02 21:52:47 +0000

    Allow any characters for Azure, let backend to do sanity check

--- aviatrix/utils.go
+++ aviatrix/utils.go
@@ -117,7 +117,7 @@ func stringInSlice(needle string, haystack []string) bool {
 
 var (
 	awsTagMatcher   = regexp.MustCompile(``) // AWS tags allow all characters
-	azureTagMatcher = regexp.MustCompile(`^[a-zA-Z0-9+\-=._ :@# ]*$`)
+	azureTagMatcher = regexp.MustCompile(``) // Azure tags allow all characters
 	gcpTagMatcher   = regexp.MustCompile(`^[\p{Ll}\p{Lo}\p{N}_-]*$`)
 )
 
