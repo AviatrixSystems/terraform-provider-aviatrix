e1bf3988b8aa5df8c9c36625a64c3b6adbcaeb0b
Sailee Rane <169497150+sairane10@users.noreply.github.com>
2024-12-04 17:29:55 -0800

    Updating the state for bgp (#2117)
    Co-authored-by: Sailee Rane <srane@avaitrix.com>

--- aviatrix/resource_aviatrix_transit_external_device_conn.go
+++ aviatrix/resource_aviatrix_transit_external_device_conn.go
@@ -1094,12 +1094,7 @@ func resourceAviatrixTransitExternalDeviceConnRead(d *schema.ResourceData, meta 
 		}
 
 		d.Set("enable_bfd", conn.EnableBfd)
-		// set the bgp_bfd config details only if the user has enabled BFD and provided the config details. For default values, the config is not set
-		bgpBfdConfig, ok := d.Get("bgp_bfd").([]interface{})
-		if !ok {
-			return fmt.Errorf("expected bgp_bfd to be a list of maps, but got %T", d.Get("bgp_bfd"))
-		}
-		if conn.EnableBfd && len(bgpBfdConfig) != 0 {
+		if conn.EnableBfd {
 			var bgpBfdConfig []map[string]interface{}
 			bfd := conn.BgpBfdConfig
 			bfdMap := make(map[string]interface{})
