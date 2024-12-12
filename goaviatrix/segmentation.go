5e0a7a9d2bfe6d1f142865e1ce3778d2e5f488bd
Sailee Rane <srane@avaitrix.com>
2024-12-11 17:44:26 -0800

    Removing the segmentation code

--- goaviatrix/segmentation.go
+++ goaviatrix/segmentation.go
@@ -28,7 +28,7 @@ func (c *Client) CreateSegmentationSecurityDomain(domain *SegmentationSecurityDo
 }
 
 func (c *Client) DeleteSegmentationSecurityDomain(domain *SegmentationSecurityDomain) error {
-	action := "_cloud_security_domain"
+	action := "delete_multi_cloud_security_domain"
 	data := map[string]interface{}{
 		"action":      action,
 		"CID":         c.CID,
