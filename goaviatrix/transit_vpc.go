e63a8ec5e00021979903ba635937a4fb87e37da7
Sailee Rane <srane@avaitrix.com>
2025-01-24 11:16:28 -0800

    Resolving the go-lint errors

--- goaviatrix/transit_vpc.go
+++ goaviatrix/transit_vpc.go
@@ -69,7 +69,7 @@ type TransitVpc struct {
 	EipMap                       string   `json:"eip_map,omitempty"`
 	LogicalEipMap                string   `json:"logical_intf_eip_map,omitempty"`
 	ZtpFileDownloadPath          string   `json:"-"`
-	ManagementEgressIpPrefix     string   `json:"mgmt_egress_ip,omitempty"`
+	ManagementEgressIPPrefix     string   `json:"mgmt_egress_ip,omitempty"`
 }
 
 type TransitGatewayAdvancedConfig struct {
