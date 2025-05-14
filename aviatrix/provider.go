f6c41a80cb4970a87eb090144f2f1377bdacd6c7
Xingwang C. <98072265+xc-6@users.noreply.github.com>
2025-05-14 12:02:25 -0700

    AVX-60903 TF support for DCF Default Action Policy (#2209)
    * AVX-60903 Add the zero trust TF template
    
    * AVX-60903 Add the zero trust TF support
    
    * AVX-60903 Fix the golint
    
    * AVX-60903 Fix the golint
    
    * AVX-60903 Fix go lint
    
    * AVX-60903 Fix the fmt
    
    * AVX-60903 go lint
    
    * AVX-60903 Added the unit test
    
    * AVX-60903 Fix the go lint
    
    * AVX-60903 Addressed comments
    
    * AVX-60903 Added the release doc
    
    * AVX-60903 Fix go lint
    
    * AVX-60903 Fix name
    
    * AVX-60903 Update the API response
    
    * AVX-60903 Fix the go unit test
    
    * AVX-60903 addressed comment

--- aviatrix/provider.go
+++ aviatrix/provider.go
@@ -111,6 +111,7 @@ func Provider() *schema.Provider {
 			"aviatrix_distributed_firewalling_intra_vpc":                      resourceAviatrixDistributedFirewallingIntraVpc(),
 			"aviatrix_distributed_firewalling_origin_cert_enforcement_config": resourceAviatrixDistributedFirewallingOriginCertEnforcementConfig(),
 			"aviatrix_distributed_firewalling_policy_list":                    resourceAviatrixDistributedFirewallingPolicyList(),
+			"aviatrix_distributed_firewalling_default_action_policy":          resourceAviatrixDistributedFirewallingDefaultActionPolicy(),
 			"aviatrix_distributed_firewalling_proxy_ca_config":                resourceAviatrixDistributedFirewallingProxyCaConfig(),
 			"aviatrix_edge_csp":                                               resourceAviatrixEdgeCSP(),
 			"aviatrix_edge_csp_ha":                                            resourceAviatrixEdgeCSPHa(),
