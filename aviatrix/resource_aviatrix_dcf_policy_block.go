378ee6640c77c9482eddca08eab13918d4f86466
Sean O'Hara <gr33nm0nk3y@gmail.com>
2025-05-19 11:39:04 -0400

    AVX-59958 TF Support for MWP (Policy Blocks)
    Added initial TF support for Policy Blocks
    
    Missing attachment points

---
+++ aviatrix/resource_aviatrix_dcf_policy_block.go
@@ -0,0 +1,207 @@
+package aviatrix
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/AviatrixSystems/terraform-provider-aviatrix/v3/goaviatrix"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceAviatrixDCFPolicyBlock() *schema.Resource {
+	return &schema.Resource{
+		CreateWithoutTimeout: resourceAviatrixDCFPolicyBlockCreate,
+		ReadWithoutTimeout:   resourceAviatrixDCFPolicyBlockRead,
+		UpdateWithoutTimeout: resourceAviatrixDCFPolicyBlockUpdate,
+		DeleteWithoutTimeout: resourceAviatrixDCFPolicyBlockDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Description: "Name of the DCF Policy Block.",
+				Required:    true,
+				Type:        schema.TypeString,
+			},
+			"policy_object": {
+				Description: "Static list of DCF Policy Blocks or Lists.",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Description: "Name of the DCF Policy Block or List.",
+							Required:    true,
+							Type:        schema.TypeString,
+						},
+						"priority": {
+							Description: "Priority of the DCF Policy Block or List.",
+							Required:    true,
+							Type:        schema.TypeInt,
+						},
+						"type": {
+							Description: "Type of DCF Policy Object ('LIST' or 'BLOCK').",
+							Required:    true,
+							Type:        schema.TypeString,
+						},
+						"uuid": {
+							Description: "UUID of the DCF Policy Block or List.",
+							Required:    true,
+							Type:        schema.TypeString,
+						},
+					},
+				},
+				Optional: true,
+				Type:     schema.TypeSet,
+			},
+		},
+	}
+}
+
+func marshalDCFPolicyBlockInput(d *schema.ResourceData) (*goaviatrix.DCFPolicyBlock, error) {
+	policyBlock := &goaviatrix.DCFPolicyBlock{}
+
+	name, ok := d.Get("name").(string)
+	if !ok {
+		return nil, fmt.Errorf("PolicyBlock name must be of type string")
+	}
+	policyBlock.Name = name
+
+	policyObjects, ok := d.Get("policy_object").(*schema.Set)
+	if !ok {
+		return nil, fmt.Errorf("Policy Block policy_object must be of type *schema.Set")
+	}
+
+	for _, policyObjectInterface := range policyObjects.List() {
+		var ok bool
+
+		policyObjectMap, ok := policyObjectInterface.(map[string]interface{})
+		if !ok {
+			return nil, fmt.Errorf("policy_object must be of type map[string]interface{}")
+		}
+
+		subPolicy, err := marshalSubPolicyInput(policyObjectMap)
+		if err != nil {
+			return nil, err
+		}
+
+		policyBlock.SubPolicies = append(policyBlock.SubPolicies, *subPolicy)
+	}
+
+	policyBlock.UUID = d.Id()
+
+	return policyBlock, nil
+}
+
+func marshalSubPolicyInput(subPolicyMap map[string]interface{}) (*goaviatrix.DCFSubPolicy, error) {
+	var ok bool
+
+	subPolicy := &goaviatrix.DCFSubPolicy{}
+
+	if subPolicy.Name, ok = subPolicyMap["name"].(string); !ok {
+		return nil, fmt.Errorf("policy_object name must be of type string.")
+	}
+
+	if subPolicy.Priority, ok = subPolicyMap["priority"].(int); !ok {
+		return nil, fmt.Errorf("policy_object priority must be of type string.")
+	}
+
+	uuid, ok := subPolicyMap["uuid"].(string)
+	if !ok {
+		return nil, fmt.Errorf("policy_object uuid must be of type string.")
+	}
+
+	subPolicyType, ok := subPolicyMap["type"].(string)
+	if !ok {
+		return nil, fmt.Errorf("policy_object type must be of type string.")
+	}
+
+	switch strings.ToUpper(subPolicyType) {
+	case "BLOCK":
+		subPolicy.Block = uuid
+	case "LIST":
+		subPolicy.List = uuid
+	default:
+		return nil, fmt.Errorf("policy_object type must be either 'BLOCK' or 'List'.")
+	}
+
+	return subPolicy, nil
+}
+
+func resourceAviatrixDCFPolicyBlockCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client, ok := meta.(*goaviatrix.Client)
+	if !ok {
+		return diag.Errorf("client must be of type *goaviatrix.Client")
+	}
+
+	policyBlock, err := marshalDCFPolicyBlockInput(d)
+	if err != nil {
+		return diag.Errorf("invalid inputs for DCF Policy during create: %s\n", err)
+	}
+
+	uuid, err := client.CreateDCFPolicyBlock(ctx, policyBlock)
+	if err != nil {
+		return diag.Errorf("failed to create DCF Policy List: %s", err)
+	}
+
+	d.SetId(uuid)
+
+	return nil
+}
+
+func resourceAviatrixDCFPolicyBlockRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client, ok := meta.(*goaviatrix.Client)
+	if !ok {
+		return diag.Errorf("client must be of type *goaviatrix.Client")
+	}
+
+	uuid := d.Id()
+
+	policyBlock, err := client.GetDCFPolicyBlock(ctx, uuid)
+	if err != nil {
+		return diag.Errorf("failed to read DCF Policy List: %s", err)
+	}
+
+	if err := d.Set("name", policyBlock.Name); err != nil {
+		return diag.Errorf("failed to set name during DCF Policy List read: %s\n", err)
+	}
+
+	d.SetId(policyBlock.UUID)
+
+	return nil
+}
+
+func resourceAviatrixDCFPolicyBlockUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client, ok := meta.(*goaviatrix.Client)
+	if !ok {
+		return diag.Errorf("client must be of type *goaviatrix.Client")
+	}
+
+	policyBlock, err := marshalDCFPolicyBlockInput(d)
+	if err != nil {
+		return diag.Errorf("invalid inputs for DCF Policy during update: %s\n", err)
+	}
+
+	err = client.UpdateDCFPolicyBlock(ctx, policyBlock)
+	if err != nil {
+		return diag.Errorf("failed to update DCF Policy List: %s", err)
+	}
+
+	return nil
+}
+
+func resourceAviatrixDCFPolicyBlockDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client, ok := meta.(*goaviatrix.Client)
+	if !ok {
+		return diag.Errorf("client must be of type *goaviatrix.Client")
+	}
+
+	uuid := d.Id()
+
+	err := client.DeleteDCFPolicyBlock(ctx, uuid)
+	if err != nil {
+		return diag.Errorf("failed to delete DCF Policy List: %v", err)
+	}
+
+	return nil
+}
---
+++ goaviatrix/dcf_policy_block.go
@@ -0,0 +1,56 @@
+package goaviatrix
+
+import (
+	"context"
+	"fmt"
+)
+
+type DCFPolicyBlock struct {
+	Name           string         `json:"name"`
+	SubPolicies    []DCFSubPolicy `json:"sub_policies"`
+	SystemResource bool           `json:"system_resource,omitempty"`
+	UUID           string         `json:"uuid,omitempty"`
+}
+
+type DCFSubPolicy struct {
+	Block    string `json:"block,omitempty"`
+	List     string `json:"list,omitempty"`
+	Name     string `json:"name"`
+	Priority int    `json:"priority"`
+	UUID     string `json:"uuid"`
+}
+
+func (c *Client) CreateDCFPolicyBlock(ctx context.Context, policyBlock *DCFPolicyBlock) (string, error) {
+	endpoint := "microseg/policy-list3"
+
+	var data DCFPolicyBlock
+	err := c.PostAPIContext25(ctx, &data, endpoint, policyBlock)
+	if err != nil {
+		return "", err
+	}
+
+	return data.UUID, nil
+}
+
+func (c *Client) GetDCFPolicyBlock(ctx context.Context, uuid string) (*DCFPolicyBlock, error) {
+	endpoint := fmt.Sprintf("microseg/policy-list3/%s", uuid)
+
+	var policyBlock DCFPolicyBlock
+	err := c.GetAPIContext25(ctx, &policyBlock, endpoint, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &policyBlock, nil
+}
+
+func (c *Client) UpdateDCFPolicyBlock(ctx context.Context, policyBlock *DCFPolicyBlock) error {
+	endpoint := fmt.Sprintf("microseg/policy-list3/%s", policyBlock.UUID)
+	return c.PutAPIContext25(ctx, endpoint, policyBlock)
+}
+
+func (c *Client) DeleteDCFPolicyBlock(ctx context.Context, uuid string) error {
+	endpoint := fmt.Sprintf("microseg/policy-list3/%s", uuid)
+	return c.DeleteAPIContext25(ctx, endpoint, nil)
+}
