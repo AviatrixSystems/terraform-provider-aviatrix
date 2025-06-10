df5e05a1b9014df643422156e5a0af5db0edd3d4
Xingwang C. <98072265+xingwang-c@users.noreply.github.com>
2025-06-10 11:41:20 -0700

    AVX-65082 Rename default action rule in TF (#2254)
    (cherry picked from commit e7b3389836a6e3c4ea5c4399196a8c95118dd6ed)

--- aviatrix/resource_aviatrix_distributed_Firewalling_default_action_rule.go
+++ aviatrix/resource_aviatrix_distributed_Firewalling_default_action_rule.go
@@ -11,12 +11,12 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
 
-func resourceAviatrixDistributedFirewallingDefaultActionPolicy() *schema.Resource {
+func resourceAviatrixDistributedFirewallingDefaultActionRule() *schema.Resource {
 	return &schema.Resource{
-		CreateWithoutTimeout: resourceAviatrixDistributedFirewallingDefaultActionPolicyCreate,
-		UpdateWithoutTimeout: resourceAviatrixDistributedFirewallingDefaultActionPolicyUpdate,
-		ReadWithoutTimeout:   resourceAviatrixDistributedFirewallingDefaultActionPolicyRead,
-		DeleteWithoutTimeout: resourceAviatrixDistributedFirewallingDefaultActionPolicyDelete,
+		CreateWithoutTimeout: resourceAviatrixDistributedFirewallingDefaultActionRuleCreate,
+		UpdateWithoutTimeout: resourceAviatrixDistributedFirewallingDefaultActionRuleUpdate,
+		ReadWithoutTimeout:   resourceAviatrixDistributedFirewallingDefaultActionRuleRead,
+		DeleteWithoutTimeout: resourceAviatrixDistributedFirewallingDefaultActionRuleDelete,
 
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
@@ -33,13 +33,13 @@ func resourceAviatrixDistributedFirewallingDefaultActionPolicy() *schema.Resourc
 			"logging": {
 				Type:        schema.TypeBool,
 				Required:    true,
-				Description: "Boolean value to enable or disable logging for the default action policy.",
+				Description: "Boolean value to enable or disable logging for the default action rule.",
 			},
 		},
 	}
 }
 
-func resourceAviatrixDistributedFirewallingDefaultActionPolicyUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func resourceAviatrixDistributedFirewallingDefaultActionRuleUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	client, ok := meta.(*goaviatrix.Client)
 	if !ok {
 		return diag.Errorf("failed to assert meta as *goaviatrix.Client")
@@ -55,20 +55,20 @@ func resourceAviatrixDistributedFirewallingDefaultActionPolicyUpdate(ctx context
 		return diag.Errorf("failed to assert 'logging' as bool")
 	}
 
-	defaultActionPolicyConfig := &goaviatrix.DistributedFirewallingDefaultActionPolicy{
+	defaultActionRuleConfig := &goaviatrix.DistributedFirewallingDefaultActionRule{
 		Action:  action,
 		Logging: logging,
 	}
 
-	if err := client.UpdateDistributedFirewallingDefaultActionPolicy(ctx, defaultActionPolicyConfig); err != nil {
-		return diag.Errorf("failed to update the default action policy: %v", err)
+	if err := client.UpdateDistributedFirewallingDefaultActionRule(ctx, defaultActionRuleConfig); err != nil {
+		return diag.Errorf("failed to update the default action rule: %v", err)
 	}
 
 	d.SetId(strings.ReplaceAll(client.ControllerIP, ".", "-"))
-	return resourceAviatrixDistributedFirewallingDefaultActionPolicyRead(ctx, d, meta)
+	return resourceAviatrixDistributedFirewallingDefaultActionRuleRead(ctx, d, meta)
 }
 
-func resourceAviatrixDistributedFirewallingDefaultActionPolicyCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func resourceAviatrixDistributedFirewallingDefaultActionRuleCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	client, ok := meta.(*goaviatrix.Client)
 	if !ok {
 		return diag.Errorf("failed to assert meta as *goaviatrix.Client")
@@ -84,21 +84,21 @@ func resourceAviatrixDistributedFirewallingDefaultActionPolicyCreate(ctx context
 		return diag.Errorf("failed to assert 'logging' as bool")
 	}
 
-	defaultActionPolicyConfig := &goaviatrix.DistributedFirewallingDefaultActionPolicy{
+	defaultActionRuleConfig := &goaviatrix.DistributedFirewallingDefaultActionRule{
 		Action:  action,
 		Logging: logging,
 	}
 
-	if err := client.UpdateDistributedFirewallingDefaultActionPolicy(ctx, defaultActionPolicyConfig); err != nil {
-		return diag.Errorf("failed to update the default action policy: %v", err)
+	if err := client.UpdateDistributedFirewallingDefaultActionRule(ctx, defaultActionRuleConfig); err != nil {
+		return diag.Errorf("failed to update the default action rule: %v", err)
 	}
 
 	d.SetId(strings.ReplaceAll(client.ControllerIP, ".", "-"))
 
-	return resourceAviatrixDistributedFirewallingDefaultActionPolicyRead(ctx, d, meta)
+	return resourceAviatrixDistributedFirewallingDefaultActionRuleRead(ctx, d, meta)
 }
 
-func resourceAviatrixDistributedFirewallingDefaultActionPolicyRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func resourceAviatrixDistributedFirewallingDefaultActionRuleRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	client, ok := meta.(*goaviatrix.Client)
 	if !ok {
 		return diag.Errorf("failed to assert meta as *goaviatrix.Client")
@@ -108,20 +108,20 @@ func resourceAviatrixDistributedFirewallingDefaultActionPolicyRead(ctx context.C
 		return diag.Errorf("ID: %s does not match controller IP %q: please provide correct ID for importing", d.Id(), client.ControllerIP)
 	}
 
-	defaultActionPolicy, err := client.GetDistributedFirewallingDefaultActionPolicy(ctx)
+	defaultActionRule, err := client.GetDistributedFirewallingDefaultActionRule(ctx)
 	if err != nil {
 		if errors.Is(err, goaviatrix.ErrNotFound) {
 			d.SetId("")
 			return nil
 		}
-		return diag.Errorf("failed to read the default action policy: %s", err)
+		return diag.Errorf("failed to read the default action rule: %s", err)
 	}
 
-	if err := d.Set("action", defaultActionPolicy.Action); err != nil {
+	if err := d.Set("action", defaultActionRule.Action); err != nil {
 		return diag.Errorf("failed to set 'action': %v", err)
 	}
 
-	if err := d.Set("logging", defaultActionPolicy.Logging); err != nil {
+	if err := d.Set("logging", defaultActionRule.Logging); err != nil {
 		return diag.Errorf("failed to set 'logging': %v", err)
 	}
 
@@ -129,19 +129,19 @@ func resourceAviatrixDistributedFirewallingDefaultActionPolicyRead(ctx context.C
 	return nil
 }
 
-func resourceAviatrixDistributedFirewallingDefaultActionPolicyDelete(ctx context.Context, _ *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func resourceAviatrixDistributedFirewallingDefaultActionRuleDelete(ctx context.Context, _ *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	client, ok := meta.(*goaviatrix.Client)
 	if !ok {
 		return diag.Errorf("failed to assert meta as *goaviatrix.Client")
 	}
 
-	defaultActionPolicyConfig := &goaviatrix.DistributedFirewallingDefaultActionPolicy{
+	defaultActionRuleConfig := &goaviatrix.DistributedFirewallingDefaultActionRule{
 		Action:  "PERMIT",
 		Logging: false,
 	}
 
-	if err := client.UpdateDistributedFirewallingDefaultActionPolicy(ctx, defaultActionPolicyConfig); err != nil {
-		return diag.Errorf("failed to update the default action policy: %v", err)
+	if err := client.UpdateDistributedFirewallingDefaultActionRule(ctx, defaultActionRuleConfig); err != nil {
+		return diag.Errorf("failed to update the default action rule: %v", err)
 	}
 
 	return nil
