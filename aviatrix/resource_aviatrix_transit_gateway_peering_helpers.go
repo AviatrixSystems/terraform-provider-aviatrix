6266a5a970982d1e7302097402ee7fb47da16106
vemurisaikartikeya <99168031+vemurisaikartikeya@users.noreply.github.com>
2025-08-28 12:36:35 -0700

    AVX-68278:  TF transit gateway peering fails for selfmanaged gateways (#2311)
    * Adding the selfmanaged gateay types for logical interfaces
    
    * Updating the comment for logical interface names
    
    ---------
    
    Co-authored-by: Sailee Rane <srane@avaitrix.com>
    (cherry picked from commit 94cd20d763327ae9751c6d2e602f5a014d1884da)

--- aviatrix/resource_aviatrix_transit_gateway_peering_helpers.go
+++ aviatrix/resource_aviatrix_transit_gateway_peering_helpers.go
@@ -48,7 +48,7 @@ func setWanInterfaceNames(
 
 	/* the cloud type here is a bit mask and we set the src/dst wan interfaces based on the cloud type
 	 * if the cloud type is Equinix or NEO, we need to convert the logical interface names from wan0, wan1 to eth0, eth1. The api for these edge types does not support logical interface names.
-	 * if the cloud type is Megaport, we need to set the logical interface names as the api for this edge type supports logical interface names.
+	 * if the cloud type is Megaport or SelfManaged, we need to set the logical interface names as the api for this edge type supports logical interface names.
 	 */
 	switch {
 	case goaviatrix.IsCloudType(cloudType, goaviatrix.EDGEEQUINIX|goaviatrix.EDGENEO):
@@ -63,7 +63,7 @@ func setWanInterfaceNames(
 		} else {
 			transitGatewayPeering.DstWanInterfaces = wanInterfacesStr
 		}
-	case goaviatrix.IsCloudType(cloudType, goaviatrix.EDGEMEGAPORT):
+	case goaviatrix.IsCloudType(cloudType, goaviatrix.EDGEMEGAPORT|goaviatrix.EDGESELFMANAGED):
 		if gatewayPrefix == "gateway1" {
 			transitGatewayPeering.Gateway1LogicalIfNames = logicalIfNames
 			log.Printf("[INFO] Gateway1 Logical Interface Names: %#v", transitGatewayPeering.Gateway1LogicalIfNames)
