70e02b4e7f6071701cf091aa99933a82ddc27489
avtx-akonnur <110423428+avtx-akonnur@users.noreply.github.com>
2025-09-18 11:42:51 -0500

    AVX-66320: Added log_profile field in TF (#2339)
    * Added log_profile field in TF
    
    * lint fix
    
    * Adding more comments.
    
    * lint fix
    
    * Added annotation for future TODOs
    
    * lint fix

--- goaviatrix/dcf_policy_list.go
+++ goaviatrix/dcf_policy_list.go
@@ -30,6 +30,7 @@ type DCFPolicy struct {
 	UUID                   string         `json:"uuid,omitempty"`
 	Watch                  bool           `json:"watch,omitempty"`
 	WebGroups              []string       `json:"web_filters,omitempty"`
+	LogProfile             string         `json:"log_profile,omitempty"`
 }
 
 type DCFPortRange struct {
