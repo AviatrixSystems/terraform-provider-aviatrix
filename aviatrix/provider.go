e333f92efaebc10ae904c6d8609ba92dc188468c
James Peach <jpeach@aviatrix.com>
2025-10-21 09:44:21 +1100

    Revert "AVX-54809: Adding a data source for webgroups to search by name (#235…" (#2401)
    This reverts commit e33bc17ccaeaa9cba932021609dbb9f6ce11a25f. That commit  broke the build by
    adding an extra definition of (c *Client) GetWebGroup().

--- aviatrix/provider.go
+++ aviatrix/provider.go
@@ -201,7 +201,6 @@ func Provider() *schema.Provider {
 			"aviatrix_account":                              dataSourceAviatrixAccount(),
 			"aviatrix_caller_identity":                      dataSourceAviatrixCallerIdentity(),
 			"aviatrix_controller_metadata":                  dataSourceAviatrixControllerMetadata(),
-			"aviatrix_web_group":                            dataSourceAviatrixDcfWebgroups(),
 			"aviatrix_device_interfaces":                    dataSourceAviatrixDeviceInterfaces(),
 			"aviatrix_edge_gateway_wan_interface_discovery": dataSourceAviatrixEdgeGatewayWanInterfaceDiscovery(),
 			"aviatrix_firenet":                              dataSourceAviatrixFireNet(),
--- goaviatrix/web_group.go
+++ goaviatrix/web_group.go
@@ -3,7 +3,6 @@ package goaviatrix
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"log"
 )
 
@@ -20,18 +19,6 @@ type WebGroup struct {
 	Name     string
 	UUID     string
 	Selector WebGroupSelector
-}
-
-type WebGroupMatchExpressionResult struct {
-	All map[string]string `json:"all"`
-}
-type WebGroupAnyResult struct {
-	Any []WebGroupMatchExpressionResult `json:"any"`
-}
-type WebGroupResult struct {
-	UUID     string            `json:"uuid"`
-	Name     string            `json:"name"`
-	Selector WebGroupAnyResult `json:"selector"`
 }
 
 func webGroupFilterToMap(filter *WebGroupMatchExpression) map[string]string {
@@ -74,74 +61,15 @@ func (c *Client) CreateWebGroup(ctx context.Context, webGroup *WebGroup) (string
 	return c.appdomainCache.Create(ctx, c, form)
 }
 
-func (c *Client) GetWebGroupByName(ctx context.Context, name string) (*WebGroup, error) {
-	endpoint := fmt.Sprintf("app-domains/name/%s", name)
-
-	var data WebGroupResult
-	err := c.GetAPIContext25(ctx, &data, endpoint, nil)
-	if err != nil {
-		return nil, err
-	}
-	if data.Name == name {
-		webGroup := &WebGroup{
-			Name: data.Name,
-			UUID: data.UUID,
-		}
-
-		for _, filterResult := range data.Selector.Any {
-			filterMap := filterResult.All
-
-			filter := &WebGroupMatchExpression{
-				SniFilter: filterMap["snifilter"],
-				UrlFilter: filterMap["urlfilter"],
-			}
-
-			webGroup.Selector.Expressions = append(webGroup.Selector.Expressions, filter)
-		}
-		return webGroup, nil
-	}
-	return nil, ErrNotFound
-}
-
 func (c *Client) GetWebGroup(ctx context.Context, uuid string) (*WebGroup, error) {
-	endpoint := fmt.Sprintf("app-domains/%s", uuid)
-
-	var data WebGroupResult
-	err := c.GetAPIContext25(ctx, &data, endpoint, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	if data.UUID == uuid {
-		webGroup := &WebGroup{
-			Name: data.Name,
-			UUID: data.UUID,
-		}
-
-		for _, filterResult := range data.Selector.Any {
-			filterMap := filterResult.All
-
-			filter := &WebGroupMatchExpression{
-				SniFilter: filterMap["snifilter"],
-				UrlFilter: filterMap["urlfilter"],
-			}
-
-			webGroup.Selector.Expressions = append(webGroup.Selector.Expressions, filter)
-		}
-		return webGroup, nil
-	}
-	return nil, ErrNotFound
-}
-
-func (c *Client) GetWebGroup(ctx context.Context, uuid string) (*WebGroup, error) {
-	endpoint := fmt.Sprintf("app-domains/%s", uuid)
-
 	type WebGroupMatchExpressionResult struct {
 		All map[string]string `json:"all"`
 	}
+
 	type WebGroupAnyResult struct {
 		Any []WebGroupMatchExpressionResult `json:"any"`
 	}
+
 	type WebGroupResult struct {
 		UUID     string            `json:"uuid"`
 		Name     string            `json:"name"`
