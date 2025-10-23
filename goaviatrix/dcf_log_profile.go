87f6e20ea1d17e8136448bfcb2808f379d3bed12
Anirudh Konnur <akonnur@aviatrix.com>
2025-10-23 00:35:12 -0500

    init commit

---
+++ goaviatrix/dcf_log_profile.go
@@ -0,0 +1,28 @@
+package goaviatrix
+
+import (
+	"context"
+	"fmt"
+)
+
+type LogProfile struct {
+	ProfileName  string `json:"profile_name"`
+	ProfileID    string `json:"profile_id"`
+	SessionEnd   bool   `json:"session_end"`
+	SessionStart bool   `json:"session_start"`
+}
+
+func (c *Client) GetLogProfileByName(ctx context.Context, profileName string) (*LogProfile, error) {
+	endpoint := fmt.Sprintf("dcf/log-profile/name/%s", profileName)
+
+	var data LogProfile
+	err := c.GetAPIContext25(ctx, &data, endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if data.ProfileName == profileName {
+		return &data, nil
+	}
+	return nil, ErrNotFound
+}
