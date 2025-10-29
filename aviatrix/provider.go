4b115886951429435e9f432c8de479bee4f0e5c3
avtx-akonnur <110423428+avtx-akonnur@users.noreply.github.com>
2025-10-29 08:25:12 -0500

    AVX-68895: Adding data source for DCF Log profile (#2409) (#2414)
    * Adding TF for trust bundles.
    
    * init commit
    
    * removing files
    
    * removed wrong files
    
    * not needed provider change
    
    * adding docs
    
    * added URL escape, unit test and updated docs
    
    * removing url escape and updated to use GET parameter
    
    * removing unit test
    
    * removing whitespace
    
    * updated to use %q
    
    (cherry picked from commit 3b1bba00fe3a332f86b2899cb03cd25164e1cd3d)

--- aviatrix/provider.go
+++ aviatrix/provider.go
@@ -213,6 +213,7 @@ func Provider() *schema.Provider {
 			"aviatrix_account":                              dataSourceAviatrixAccount(),
 			"aviatrix_caller_identity":                      dataSourceAviatrixCallerIdentity(),
 			"aviatrix_controller_metadata":                  dataSourceAviatrixControllerMetadata(),
+			"aviatrix_dcf_log_profile":                      dataSourceAviatrixDcfLogProfile(),
 			"aviatrix_dcf_mwp_attachment_point":             dataSourceAviatrixDcfMwpAttachmentPoints(),
 			"aviatrix_web_group":                            dataSourceAviatrixDcfWebgroups(),
 			"aviatrix_device_interfaces":                    dataSourceAviatrixDeviceInterfaces(),
