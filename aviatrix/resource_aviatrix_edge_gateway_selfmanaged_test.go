30b9fc22817a7d9774b5f6c5e652b579e870e538
Punit Kandoi <pkandoi@aviatrix.com>
2025-10-29 20:08:05 +0530

    Added test case.

--- aviatrix/resource_aviatrix_edge_gateway_selfmanaged_test.go
+++ aviatrix/resource_aviatrix_edge_gateway_selfmanaged_test.go
@@ -45,6 +45,18 @@ func TestAccAviatrixEdgeGatewaySelfmanaged_basic(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "bgp_neighbor_status_polling_time", "5"),
 					resource.TestCheckResourceAttr(resourceName, "included_advertised_spoke_routes.0", "10.230.3.0/24"),
 					resource.TestCheckResourceAttr(resourceName, "included_advertised_spoke_routes.1", "10.230.5.0/24"),
+					resource.TestCheckResourceAttr(resourceName, "tunnel_encryption_cipher", "strong"),
+					resource.TestCheckResourceAttr(resourceName, "tunnel_forward_secrecy", "enable"),
+				),
+			},
+			{
+				Config: testAccEdgeGatewaySelfmanagedUpdate(gwName, siteId, path),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckEdgeGatewaySelfmanagedExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "gw_name", gwName),
+					resource.TestCheckResourceAttr(resourceName, "site_id", siteId),
+					resource.TestCheckResourceAttr(resourceName, "tunnel_encryption_cipher", "default"),
+					resource.TestCheckResourceAttr(resourceName, "tunnel_forward_secrecy", "disable"),
 				),
 			},
 			{
@@ -66,6 +78,52 @@ resource "aviatrix_edge_gateway_selfmanaged" "test" {
 	ztp_file_download_path             = "%s"
 	bgp_polling_time                   = 50
 	bgp_neighbor_status_polling_time   = 5
+	tunnel_encryption_cipher           = "strong"
+	tunnel_forward_secrecy             = "enable"
+
+	interfaces {
+		name          = "eth0"
+		type          = "WAN"
+		ip_address    = "10.230.5.32/24"
+		gateway_ip    = "10.230.5.100"
+		wan_public_ip = "64.71.24.221"
+		dns_server_ip = "8.8.8.8"
+		secondary_dns_server_ip = "9.9.9.9"
+	}
+
+	interfaces {
+		name       = "eth1"
+		type       = "LAN"
+		ip_address = "10.230.3.32/24"
+	}
+
+	interfaces {
+		name        = "eth2"
+		type        = "MANAGEMENT"
+		enable_dhcp = false
+		ip_address  = "172.16.15.162/20"
+		gateway_ip  = "172.16.0.1"
+	}
+
+	included_advertised_spoke_routes = [
+		"10.230.3.0/24",
+		"10.230.5.0/24"
+	]
+}
+  `, gwName, siteId, path)
+}
+
+func testAccEdgeGatewaySelfmanagedUpdate(gwName, siteId, path string) string {
+	return fmt.Sprintf(`
+resource "aviatrix_edge_gateway_selfmanaged" "test" {
+	gw_name                            = "%s"
+	site_id                            = "%s"
+	ztp_file_type                      = "iso"
+	ztp_file_download_path             = "%s"
+	bgp_polling_time                   = 50
+	bgp_neighbor_status_polling_time   = 5
+	tunnel_encryption_cipher           = "default"
+	tunnel_forward_secrecy             = "disable"
 
 	interfaces {
 		name          = "eth0"
@@ -137,4 +195,90 @@ func testAccCheckEdgeGatewaySelfmanagedDestroy(s *terraform.State) error {
 	}
 
 	return nil
+}
+
+func TestAccAviatrixEdgeGatewaySelfmanaged_tunnelPolicies(t *testing.T) {
+	if os.Getenv("SKIP_EDGE_GATEWAY_SELFMANAGED") == "yes" {
+		t.Skip("Skipping Edge Gateway Selfmanaged test as SKIP_EDGE_GATEWAY_SELFMANAGED is set")
+	}
+
+	resourceName := "aviatrix_edge_gateway_selfmanaged.test_tunnel"
+	gwName := "edge-tunnel-" + acctest.RandString(5)
+	siteId := "site-tunnel-" + acctest.RandString(5)
+	path, _ := os.Getwd()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckEdgeGatewaySelfmanagedDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccEdgeGatewaySelfmanagedTunnelPoliciesConfig(gwName, siteId, path, "default", "disable"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckEdgeGatewaySelfmanagedExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "gw_name", gwName),
+					resource.TestCheckResourceAttr(resourceName, "site_id", siteId),
+					resource.TestCheckResourceAttr(resourceName, "tunnel_encryption_cipher", "default"),
+					resource.TestCheckResourceAttr(resourceName, "tunnel_forward_secrecy", "disable"),
+				),
+			},
+			{
+				Config: testAccEdgeGatewaySelfmanagedTunnelPoliciesConfig(gwName, siteId, path, "strong", "enable"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckEdgeGatewaySelfmanagedExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "gw_name", gwName),
+					resource.TestCheckResourceAttr(resourceName, "site_id", siteId),
+					resource.TestCheckResourceAttr(resourceName, "tunnel_encryption_cipher", "strong"),
+					resource.TestCheckResourceAttr(resourceName, "tunnel_forward_secrecy", "enable"),
+				),
+			},
+			{
+				Config: testAccEdgeGatewaySelfmanagedTunnelPoliciesConfig(gwName, siteId, path, "default", "disable"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckEdgeGatewaySelfmanagedExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "gw_name", gwName),
+					resource.TestCheckResourceAttr(resourceName, "site_id", siteId),
+					resource.TestCheckResourceAttr(resourceName, "tunnel_encryption_cipher", "default"),
+					resource.TestCheckResourceAttr(resourceName, "tunnel_forward_secrecy", "disable"),
+				),
+			},
+		},
+	})
+}
+
+func testAccEdgeGatewaySelfmanagedTunnelPoliciesConfig(gwName, siteId, path, encCipher, forwardSecrecy string) string {
+	return fmt.Sprintf(`
+resource "aviatrix_edge_gateway_selfmanaged" "test_tunnel" {
+	gw_name                            = "%s"
+	site_id                            = "%s"
+	ztp_file_type                      = "iso"
+	ztp_file_download_path             = "%s"
+	tunnel_encryption_cipher           = "%s"
+	tunnel_forward_secrecy             = "%s"
+
+	interfaces {
+		name          = "eth0"
+		type          = "WAN"
+		ip_address    = "10.230.5.32/24"
+		gateway_ip    = "10.230.5.100"
+		wan_public_ip = "64.71.24.221"
+	}
+
+	interfaces {
+		name       = "eth1"
+		type       = "LAN"
+		ip_address = "10.230.3.32/24"
+	}
+
+	interfaces {
+		name        = "eth2"
+		type        = "MANAGEMENT"
+		enable_dhcp = false
+		ip_address  = "172.16.15.162/20"
+		gateway_ip  = "172.16.0.1"
+	}
+}
+  `, gwName, siteId, path, encCipher, forwardSecrecy)
 }
