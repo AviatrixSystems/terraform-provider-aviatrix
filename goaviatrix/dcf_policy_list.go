b12e08445678efb8a6b55dd32f337e4c6ac027a4
Anirudh Konnur <akonnur@aviatrix.com>
2025-11-17 15:52:36 -0600

    adding metadata fields for TF created policy objects.

--- goaviatrix/dcf_policy_list.go
+++ goaviatrix/dcf_policy_list.go
@@ -6,31 +6,33 @@ import (
 )
 
 type DCFPolicyList struct {
-	AttachTo       string      `json:"attach_to,omitempty"`
-	Name           string      `json:"name"`
-	Policies       []DCFPolicy `json:"policies"`
-	SystemResource bool        `json:"system_resource,omitempty"`
-	UUID           string      `json:"uuid,omitempty"`
+	AttachTo       string                 `json:"attach_to,omitempty"`
+	Name           string                 `json:"name"`
+	Policies       []DCFPolicy            `json:"policies"`
+	SystemResource bool                   `json:"system_resource,omitempty"`
+	UUID           string                 `json:"uuid,omitempty"`
+	Metadata       map[string]interface{} `json:"metadata,omitempty"`
 }
 
 type DCFPolicy struct {
-	Action                 string         `json:"action"`
-	DecryptPolicy          string         `json:"decrypt_policy,omitempty"`
-	DstSmartGroups         []string       `json:"dst_ads"`
-	ExcludeSgOrchestration bool           `json:"exclude_sg_orchestration,omitempty"`
-	FlowAppRequirement     string         `json:"flow_app_requirement,omitempty"`
-	Logging                bool           `json:"logging,omitempty"`
-	Name                   string         `json:"name"`
-	PortRanges             []DCFPortRange `json:"port_ranges,omitempty"`
-	Priority               int            `json:"priority"`
-	Protocol               string         `json:"protocol"`
-	SrcSmartGroups         []string       `json:"src_ads"`
-	SystemResource         bool           `json:"system_resource,omitempty"`
-	TLSProfile             string         `json:"tls_profile,omitempty"`
-	UUID                   string         `json:"uuid,omitempty"`
-	Watch                  bool           `json:"watch,omitempty"`
-	WebGroups              []string       `json:"web_filters,omitempty"`
-	LogProfile             string         `json:"log_profile,omitempty"`
+	Action                 string                 `json:"action"`
+	DecryptPolicy          string                 `json:"decrypt_policy,omitempty"`
+	DstSmartGroups         []string               `json:"dst_ads"`
+	ExcludeSgOrchestration bool                   `json:"exclude_sg_orchestration,omitempty"`
+	FlowAppRequirement     string                 `json:"flow_app_requirement,omitempty"`
+	Logging                bool                   `json:"logging,omitempty"`
+	Name                   string                 `json:"name"`
+	PortRanges             []DCFPortRange         `json:"port_ranges,omitempty"`
+	Priority               int                    `json:"priority"`
+	Protocol               string                 `json:"protocol"`
+	SrcSmartGroups         []string               `json:"src_ads"`
+	SystemResource         bool                   `json:"system_resource,omitempty"`
+	TLSProfile             string                 `json:"tls_profile,omitempty"`
+	UUID                   string                 `json:"uuid,omitempty"`
+	Watch                  bool                   `json:"watch,omitempty"`
+	WebGroups              []string               `json:"web_filters,omitempty"`
+	LogProfile             string                 `json:"log_profile,omitempty"`
+	Metadata               map[string]interface{} `json:"metadata,omitempty"`
 }
 
 type DCFPortRange struct {
@@ -40,7 +42,18 @@ type DCFPortRange struct {
 
 func (c *Client) CreateDCFPolicyList(ctx context.Context, policyList *DCFPolicyList) (string, error) {
 	endpoint := "microseg/policy-list3"
-
+	policyList.Metadata = map[string]interface{}{
+		"terraform": map[string]string{
+			"resource_type": "terraform-policy-list",
+		},
+	}
+	for i := range policyList.Policies {
+		policyList.Policies[i].Metadata = map[string]interface{}{
+			"terraform": map[string]string{
+				"resource_type": "terraform-policy",
+			},
+		}
+	}
 	var data DCFPolicyList
 	err := c.PostAPIContext25(ctx, &data, endpoint, policyList)
 	if err != nil {
